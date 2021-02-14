420d99179548424d35ccef64f70ea0255f96cda3
Martin Roa Villescas <m.roa.villescas@tue.nl>
2021-02-14 18:48:32 +0100

    vim: add headers to LSP config

--- nvim/.config/nvim/lua/mrv/lsp/init.lua
+++ nvim/.config/nvim/lua/mrv/lsp/init.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+---------------------------------------------------------------------------------
+--- General
+---------------------------------------------------------------------------------
+
 function M.goto_next()
   vim.lsp.diagnostic.goto_next{ wrap = false,}
 end
@@ -8,54 +12,71 @@ function M.goto_prev()
   vim.lsp.diagnostic.goto_prev{ wrap = false,}
 end
 
-if not os.getenv("SSH_CONNECTION") then
+local lspconfig = require'lspconfig'
 
-  local lspconfig = require'lspconfig'
+-- Check if this works (can I get rid of the snippets plugin?)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-  -- check if this works (can I get rid of the snippets plugin?)
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, {
-      underline = false,
-      virtual_text = true,
-      signs = true,
-      update_in_insert = false,
-    }
-  )
-
-  lspconfig.vimls.setup{}
-  lspconfig.jedi_language_server.setup{}
-  -- lspconfig.bashls.setup{}
-  -- lspconfig.texlab.setup{}
-  -- lspconfig.cmake.setup{}
-
-  lspconfig.clangd.setup{
-    cmd = { "clangd", "--background-index", "--fallback-style=LLVM" },
-    filetypes = { "c", "cpp", "objc", "objcpp"},
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
   }
+)
 
-  local lua_lsp_loc = "/home/mroavi/lsp-servers/lua-language-server/"
-  lspconfig.sumneko_lua.setup{
-    cmd = {lua_lsp_loc .. "bin/Linux/lua-language-server", "-E", lua_lsp_loc .. "/main.lua"},
-    capabilities = capabilities,
-    settings = {
-      Lua = {
-        runtime = {version = "LuaJIT", path = vim.split(package.path, ";")},
-        diagnostics = {globals = {"vim"}},
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
-          }
+---------------------------------------------------------------------------------
+--- LSP Servers Config
+---------------------------------------------------------------------------------
+
+--- vim ------------------------------------------------------------------------
+
+lspconfig.vimls.setup{}
+
+--- python ---------------------------------------------------------------------
+
+lspconfig.jedi_language_server.setup{}
+
+--- c --------------------------------------------------------------------------
+
+lspconfig.clangd.setup{
+  cmd = { "clangd", "--background-index", "--fallback-style=LLVM" },
+  filetypes = { "c", "cpp", "objc", "objcpp"},
+}
+
+--- lua ------------------------------------------------------------------------
+
+local lua_lsp_dir = "/home/mroavi/lsp-servers/lua-language-server/"
+lspconfig.sumneko_lua.setup{
+  cmd = {lua_lsp_dir .. "bin/Linux/lua-language-server", "-E", lua_lsp_dir .. "/main.lua"},
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {version = "LuaJIT", path = vim.split(package.path, ";")},
+      diagnostics = {globals = {"vim"}},
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true
         }
       }
     }
   }
+}
 
-end
+-- <disabled> ------------------------------------------------------------------
+
+-- lspconfig.bashls.setup{}
+-- lspconfig.texlab.setup{}
+-- lspconfig.cmake.setup{}
+
+-- -- Example of how to run code depending on a environment variable
+-- if not os.getenv("SSH_CONNECTION") then
+--   -- <CODE>
+-- end
 
 return M
 
