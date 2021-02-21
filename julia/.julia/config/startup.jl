2c4a56ad81f30906681c5b7d8d0fd4a41fa73981
Martin Roa Villescas <m.roa.villescas@tue.nl>
2021-02-21 13:46:18 +0100

    julia: add tips and tricks to startup.jl

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -14,3 +14,50 @@ function clr()
     println()
   end
 end
+
+
+#=----------------------------------------------------------------------------=#
+# JULIA TIPS AND TRICKS
+#=----------------------------------------------------------------------------=#
+
+#=----------------------------- List of Handy Macros ---------------------------
+
+  @which <function/macro>
+  @edit <function/macro>
+  @debug message  [key=value | value ...]
+
+  @code_lowered   <function/macro>
+  @code_typed     <function/macro>
+  @code_llvm      <function/macro>
+  @code_native    <function/macro>
+  @code_warntype  <function/macro>
+
+To enable @debug messages, you need to set the JULIA_DEBUG ENV var:
+
+  julia> ENV["JULIA_DEBUG"] = "all"
+
+
+----------------------------------- REPL History -------------------------------
+
+Type the beginning of a command and type Ctrl-p to browse through all commands
+in history that begin with the same chars. For example:
+
+  julia> ENV<CTRL+P>
+
+completes to:
+
+  julia> ENV["JULIA_DEBUG"] = "all"
+
+Type Ctrl-R in the REPL to start FZF to filter the REPL History
+
+
+# ----------------------------------- Debugger ---------------------------------
+
+  julia> using Debugger       # import the julia debugger
+  julia> @enter <function>    # run <function> in debugger mode
+  debug> ?                    # list the help to see all debugger available cmds
+  debug> `                    # go to julia mode keeping the backtrace (backspace to leave)
+  |julia> print(x)            # print the content of `x`
+  |julia> x = 2               # assign the value 2 to `x`
+
+------------------------------------------------------------------------------=#
