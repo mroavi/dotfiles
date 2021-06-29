903087db5a60630771254641107d3d878eb3d15a
Martin Roa Villescas <mroavi@gmail.com>
2021-06-29 17:36:01 +0200

    vim: minor

--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -1,4 +1,4 @@
 require('mrv.plugins.telescope')
 require('mrv.plugins.lspconfig')
-require('mrv.plugins.compe')
+require('mrv.plugins.compe') -- messes up nvim's intro screen
 require('mrv.plugins.gitsigns')
