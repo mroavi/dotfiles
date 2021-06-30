5557fd60e99a398098fc1f5c84926d14d2ab65db
Martin Roa Villescas <mroavi@gmail.com>
2021-06-30 18:34:37 +0200

    vim: remove scroll by a third of win height
    and disable automatic comment insertion

--- nvim/.config/nvim/lua/mrv/settings.lua
+++ nvim/.config/nvim/lua/mrv/settings.lua
@@ -20,7 +20,6 @@ local options = {
 	linebreak = true,
 	list = false,
 	hidden = true,
-	-- "set formatoptions-=tc " disable auto-wrap text using textwidth
 	formatoptions = 'tcqj',
 	splitbelow = true,
 	splitright = true,
@@ -43,13 +42,6 @@ local options = {
 M.setup = function()
 	apply_options(globals, vim.g)
   apply_options(options, vim.o)
-
-	-- Disable automatic comment insertion (https://superuser.com/a/271024/1087113)
-	vim.cmd [[ autocmd BufNewFile,BufRead * setlocal formatoptions-=cro ]]
-
-	-- -- Scroll by a third of window height (https://stackoverflow.com/a/16574696/1706778)
-	-- vim.cmd("execute 'set scroll=' .&lines / 3")
-	-- vim.cmd("au VimResized * execute 'set scroll=' . &lines / 3")
 
   vim.cmd [[ colorscheme marlin ]]
 
