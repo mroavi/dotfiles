93932c60dd842d9bb12e7658ed9bd4d17b3eb666
Martin Roa Villescas <mroavi@gmail.com>
2021-06-30 15:04:23 +0200

    vim: minor

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,4 @@
 -- Vi(m) philosophy: https://stackoverflow.com/a/1220118/1706778
-
 require('mrv.settings').setup()
 require('mrv.plugins')
 vim.cmd("exe 'source /home/mroavi/.config/nvim/lua/mrv/custom.vim'")
