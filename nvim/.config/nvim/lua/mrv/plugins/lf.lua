73b60e50233afe39327e315e4aa0fc61fd969e5c
Martin Roa Villescas <mroavi@gmail.com>
2021-07-02 01:13:12 +0200

    vim: use lua style of setting global settings with a hash in the name

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -1,8 +1,12 @@
-vim.cmd("let g:lf#set_default_mappings = 0")
-vim.cmd("let g:lf#replace_netrw = 1")
-vim.cmd("let g:lf#layout = { 'window': { 'width': 0.9, 'height': 0.9, 'highlight': 'Normal' } }")
-vim.cmd("let g:lf#action = { '<c-t>': 'tab split', '<c-x>': 'split', '<c-v>': 'vsplit' }")
+vim.g["g:lf#set_default_mappings"] = 0
+vim.g["let g:lf#replace_netrw"] = 1
+vim.g["g:lf#layout"] = { window = { width = 0.9,
+																		height = 0.9,
+																		highlight = "Normal", } }
+vim.g['lf#action'] = { ['<C-t>'] = 'tab split',
+											 ['<C-x>'] = 'split',
+											 ['<C-v>'] = 'vsplit', }
 
-local utils = require('mrv.utils')
+local utils = require("mrv.utils")
 utils.remap("n", "<Leader>/", ":LfPicker %:p:h<CR>")
 
