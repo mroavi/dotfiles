1ca4828f10c74f1979a52555c9637f7f978f9749
Martin Roa Villescas <mroavi@gmail.com>
2021-07-01 23:50:37 +0200

    vim: restructure plugins init file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,6 @@
 --    |_|_| |_|_|\__(_)_|\__,_|\__,_|
 
 require('mrv.settings').setup()
-require('mrv.plugins')
+require('mrv.plugins').setup()
 vim.cmd [[ exe "source " .. stdpath('config') .. "/lua/mrv/custom.vim" ]]
 
