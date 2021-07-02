8a305af22ceb1dfc079266831f7e1c1beedf1e9d
Martin Roa Villescas <mroavi@gmail.com>
2021-07-02 11:16:02 +0200

    vim: fix lf.lua mappings

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -1,7 +1,7 @@
-vim.g["g:lf#set_default_mappings"] = 0
-vim.g["let g:lf#replace_netrw"] = 1
-vim.g["g:lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
-vim.g['lf#action'] = { ['<C-t>'] = 'tab split',
+vim.g["lf#set_default_mappings"] = 0
+vim.g["lf#replace_netrw"] = 1
+vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
+vim.g["lf#action"] = { ['<C-t>'] = 'tab split',
 											 ['<C-x>'] = 'split',
 											 ['<C-v>'] = 'vsplit', }
 
