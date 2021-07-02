e6a5a4ca30d73455b73f3c55b9f8eee1425fbe85
Martin Roa Villescas <mroavi@gmail.com>
2021-07-02 17:39:55 +0200

    vim: resource mrv after reloading all modules with plenary

--- nvim/.config/nvim/lua/mrv/utils.lua
+++ nvim/.config/nvim/lua/mrv/utils.lua
@@ -7,6 +7,7 @@ end
 M.reload = function()
   print("Configuration reloaded")
   require("plenary.reload").reload_module("mrv")
+	require("mrv")
 end
 
 M.remap("n", "<Leader>re", "<Cmd>lua require('mrv.utils').reload('mrv')<CR>")
