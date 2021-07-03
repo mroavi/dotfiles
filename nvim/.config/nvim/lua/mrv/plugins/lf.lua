d768fa7ff632b031f71a207d8cb99e719a25197b
Martin Roa Villescas <mroavi@gmail.com>
2021-07-03 23:56:54 +0200

    vim: replace 0 and 1 with true and false

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -1,5 +1,5 @@
-vim.g["lf#set_default_mappings"] = 0
-vim.g["lf#replace_netrw"] = 1
+vim.g["lf#set_default_mappings"] = false
+vim.g["lf#replace_netrw"] = true
 vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
 vim.g["lf#action"] = { ['<C-t>'] = 'tab split',
 											 ['<C-x>'] = 'split',
