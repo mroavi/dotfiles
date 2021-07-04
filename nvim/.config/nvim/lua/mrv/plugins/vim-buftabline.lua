bbb732b6073a18825d49b90381415f4453f6246f
Martin Roa Villescas <mroavi@gmail.com>
2021-07-04 18:22:53 +0200

    vim: add vim-buftabline plugin but disabled

---
+++ nvim/.config/nvim/lua/mrv/plugins/vim-buftabline.lua
@@ -0,0 +1,13 @@
+vim.g.buftabline_numbers = 2
+vim.api.nvim_command([[
+nmap <Leader>1 <Plug>BufTabLine.Go(1)
+nmap <Leader>2 <Plug>BufTabLine.Go(2)
+nmap <Leader>3 <Plug>BufTabLine.Go(3)
+nmap <Leader>4 <Plug>BufTabLine.Go(4)
+nmap <Leader>5 <Plug>BufTabLine.Go(5)
+nmap <Leader>6 <Plug>BufTabLine.Go(6)
+nmap <Leader>7 <Plug>BufTabLine.Go(7)
+nmap <Leader>8 <Plug>BufTabLine.Go(8)
+nmap <Leader>9 <Plug>BufTabLine.Go(9)
+]])
+
