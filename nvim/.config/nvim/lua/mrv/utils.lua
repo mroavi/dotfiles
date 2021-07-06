b0b172ed496b401fe9c9e466434b87b980a1c608
Martin Roa Villescas <mroavi@gmail.com>
2021-07-06 23:43:23 +0200

    vim: add function to remap buffer-local keybindings

--- nvim/.config/nvim/lua/mrv/utils.lua
+++ nvim/.config/nvim/lua/mrv/utils.lua
@@ -1,7 +1,13 @@
 local M = {}
 
+-- Global mappings
 M.remap = function(mode, lhs, rhs, opts)
 	vim.api.nvim_set_keymap(mode, lhs, rhs, opts or {noremap = true})
+end
+
+-- Buffer-local mappings
+M.remap_buff = function(mode, lhs, rhs, opts)
+	vim.api.nvim_buf_set_keymap(mode, lhs, rhs, opts or {noremap = true})
 end
 
 M.reload = function()
