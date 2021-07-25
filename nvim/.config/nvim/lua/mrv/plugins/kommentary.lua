845f73e4363bc05d85ca123f6ea1ec99e15462af
Martin Roa Villescas <mroavi@gmail.com>
2021-07-25 23:51:05 +0200

    vim: remove commented out code

--- nvim/.config/nvim/lua/mrv/plugins/kommentary.lua
+++ nvim/.config/nvim/lua/mrv/plugins/kommentary.lua
@@ -3,8 +3,3 @@ require('kommentary.config').configure_language("default", {
     prefer_single_line_comments = true,
 })
 
--- -- Specific language configuration
--- require('kommentary.config').configure_language("lua", {
--- 		prefer_single_line_comments = true,
--- })
-
