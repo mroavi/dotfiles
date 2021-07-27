1714623d2fb1a9fd873a4bc12e89a10ff6f170de
Martin Roa Villescas <mroavi@gmail.com>
2021-07-27 12:28:36 +0200

    vim: remove escape after commenting in visual mode
    This became the default now.

--- nvim/.config/nvim/lua/mrv/plugins/kommentary.lua
+++ nvim/.config/nvim/lua/mrv/plugins/kommentary.lua
@@ -3,10 +3,3 @@ require('kommentary.config').configure_language("default", {
     prefer_single_line_comments = true,
 })
 
-vim.g.kommentary_create_default_mappings = false
-
-local utils = require('mrv.utils')
-utils.remap("n", "gcc", "<Plug>kommentary_line_default", {noremap = false})
-utils.remap("n", "gc", "<Plug>kommentary_motion_default", {noremap = false})
-utils.remap("x", "gc", "<Plug>kommentary_visual_default<Esc>", {noremap = false}) -- escape after commenting in visual mode
-
