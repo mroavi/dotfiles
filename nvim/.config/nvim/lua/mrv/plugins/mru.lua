b5dedbcdaa7f3eccecc976325515aae6f8bf003e
Martin Roa Villescas <mroavi@gmail.com>
2021-07-30 12:23:13 +0200

    vim: increase MRU window height

--- nvim/.config/nvim/lua/mrv/plugins/mru.lua
+++ nvim/.config/nvim/lua/mrv/plugins/mru.lua
@@ -1,6 +1,7 @@
 vim.g.MRU_File = "$HOME/.vim_mru_files"
 vim.g.MRU_Max_Entries = 500
 vim.g.MRU_Exclude_Files = "\\.git"
+vim.g.MRU_Window_Height = 15
 
 local utils = require('mrv.utils')
 utils.remap("n", "<Leader>mr", "<Cmd>MRU<CR>", {silent = true})
