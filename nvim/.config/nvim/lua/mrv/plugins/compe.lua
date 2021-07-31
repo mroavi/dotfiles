bbb278ed0394efe199af811a910f38e0b07b3129
Martin Roa Villescas <mroavi@gmail.com>
2021-07-31 18:18:31 +0200

    vim: refactor lspconfig config file

--- nvim/.config/nvim/lua/mrv/plugins/compe.lua
+++ nvim/.config/nvim/lua/mrv/plugins/compe.lua
@@ -1,72 +1,66 @@
 local M = {}
 
 M.setup = function()
-  require'compe'.setup {
-    enabled = true;
-    autocomplete = true;
-    debug = false;
-    min_length = 1;
-    preselect = 'enable';
-    throttle_time = 80;
-    source_timeout = 200;
-    incomplete_delay = 400;
-    max_abbr_width = 100;
-    max_kind_width = 100;
-    max_menu_width = 100;
-    documentation = true;
-    source = {
-      path = true;
-      buffer = true;
-      calc = true;
-      nvim_lsp = true;
-      nvim_lua = true;
-      vsnip = true;
+	require'compe'.setup {
+		source = {
+			path = true;
+			buffer = true;
+			calc = true;
+			nvim_lsp = true;
+			nvim_lua = true;
+			vsnip = true;
+			ultisnips = false;
+			luasnip = false;
 			spell = false;
-    };
-  }
+		};
+	}
 end
 
+---------------------------------------------------------------------------------
+-- How to use tab to navigate completion menu? (from README)
+---------------------------------------------------------------------------------
+
 local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
+	return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
+	local col = vim.fn.col('.') - 1
+	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
 -- Use (s-)tab to:
 --- move to prev/next item in completion menuone
 --- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-n>"
+	elseif vim.fn['vsnip#available'](1) == 1 then
+		return t "<Plug>(vsnip-expand-or-jump)"
+	elseif check_back_space() then
+		return t "<Tab>"
+	else
+		return vim.fn['compe#complete']()
+	end
 end
 _G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    -- If <S-Tab> is not working in your terminal, change it to <C-h>
-    return t "<S-Tab>"
-  end
+	if vim.fn.pumvisible() == 1 then
+		return t "<C-p>"
+	elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+		return t "<Plug>(vsnip-jump-prev)"
+	else
+		-- If <S-Tab> is not working in your terminal, change it to <C-h>
+		return t "<S-Tab>"
+	end
 end
+
+---------------------------------------------------------------------------------
+-- Mappings
+---------------------------------------------------------------------------------
 
 local utils = require('mrv.utils')
 
-utils.remap("i", "<C-Space>", "compe#complete()", {expr = true}) -- trigger completion menu
+utils.remap("i", "<C-Space>", "compe#complete()", {expr = true, noremap = true}) -- trigger completion menu
 utils.remap("i", "<CR>", "compe#confirm('<CR>')", {expr = true, noremap = true}) -- select option from menu
 -- utils.remap("i", "<CR>", "compe#confirm({ 'keys': \"\\<Plug>delimitMateCR\", 'mode': '' })", {expr = true}) -- compatitbility with DelimitMate
 -- vim.cmd [[ inoremap <silent><expr> <CR> compe#confirm(luaeval("require 'nvim-autopairs'.autopairs_cr()")) ]] -- compatitbility with nvim-autopairs
