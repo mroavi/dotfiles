475ac7b13a8259cb4a8020506ba4169f0fa823e7
Martin Roa Villescas <mroavi@gmail.com>
2021-08-03 16:24:28 +0200

    vim: convert tabs to spaces

--- nvim/.config/nvim/lua/mrv/settings.lua
+++ nvim/.config/nvim/lua/mrv/settings.lua
@@ -9,48 +9,48 @@ local apply_options = function(opts, endpoint)
 end
 
 local globals = {
-	mapleader = ' ',
+  mapleader = ' ',
 }
 
 local options = {
-	shada = "<10,'99,/20,:99,h,f1",
-	number = false,
-	relativenumber = false,
-	swapfile = false,
-	wrap = true,
-	linebreak = true,
-	list = false,
-	hidden = true,
-	formatoptions = 'tcqj',
-	splitbelow = true,
-	splitright = true,
-	signcolumn = 'yes',
-	tabstop = 2,
-	softtabstop = 2,
-	shiftwidth = 2,
-	shiftround = true,
-	expandtab = true,
-	smartindent = true,
-	updatetime = 100,
-	inccommand = 'split',
-	listchars = "tab:»\\ ,space:_,trail:·,eol:¬",
-	scrolloff = 5,
-	completeopt = 'menuone,noselect',
-	clipboard = 'unnamedplus',
-	termguicolors = true,
-	background = 'dark',
-	wildignorecase = true,
+  shada = "<10,'99,/20,:99,h,f1",
+  number = false,
+  relativenumber = false,
+  swapfile = false,
+  wrap = true,
+  linebreak = true,
+  list = false,
+  hidden = true,
+  formatoptions = 'tcqj',
+  splitbelow = true,
+  splitright = true,
+  signcolumn = 'yes',
+  tabstop = 2,
+  softtabstop = 2,
+  shiftwidth = 2,
+  shiftround = true,
+  expandtab = true,
+  smartindent = true,
+  updatetime = 100,
+  inccommand = 'split',
+  listchars = "tab:»\\ ,space:_,trail:·,eol:¬",
+  scrolloff = 5,
+  completeopt = 'menuone,noselect',
+  clipboard = 'unnamedplus',
+  termguicolors = true,
+  background = 'dark',
+  wildignorecase = true,
 }
 
 M.setup = function()
 
-	apply_options(globals, vim.g)
+  apply_options(globals, vim.g)
   apply_options(options, vim.o)
 
-	-- Make Ctrl-u and Ctrl-d scroll 1/3 of the window height
-	-- https://neovim.discourse.group/t/how-to-make-ctrl-d-and-ctrl-u-scroll-1-3-of-window-height/859
-	utils.remap("n", "<C-d>", "(winheight(0) / 3) . '<C-d>'", {noremap = true, expr = true})
-	utils.remap("n", "<C-u>", "(winheight(0) / 3) . '<C-u>'", {noremap = true, expr = true})
+  -- Make Ctrl-u and Ctrl-d scroll 1/3 of the window height
+  -- https://neovim.discourse.group/t/how-to-make-ctrl-d-and-ctrl-u-scroll-1-3-of-window-height/859
+  utils.remap("n", "<C-d>", "(winheight(0) / 3) . '<C-d>'", {noremap = true, expr = true})
+  utils.remap("n", "<C-u>", "(winheight(0) / 3) . '<C-u>'", {noremap = true, expr = true})
 
 end
 
