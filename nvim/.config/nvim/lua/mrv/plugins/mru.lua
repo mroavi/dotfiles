efd54573d69d0c12274879a3eef55555eab7d952
Martin Roa Villescas <mroavi@gmail.com>
2021-08-04 17:23:57 +0200

    vim: show dir + filename in MRU list

--- nvim/.config/nvim/lua/mrv/plugins/mru.lua
+++ nvim/.config/nvim/lua/mrv/plugins/mru.lua
@@ -6,3 +6,7 @@ vim.g.MRU_Window_Height = 15
 local utils = require('mrv.utils')
 utils.remap("n", "<Leader>mr", "<Cmd>MRU<CR>j", {silent = true})
 
+vim.cmd [[
+let MRU_Filename_Format = { 'formatter': "fnamemodify(v:val, ':p:h:t') .. '/' .. fnamemodify(v:val, ':p:t') .. ' (' .. v:val .. ')'", 'parser':'(\zs.*\ze)', 'syntax': '^.\{-}\ze('}
+]]
+
