5222ba2c402f78af96baf22216aadb577d91097c
Martin Roa Villescas <mroavi@gmail.com>
2021-08-06 14:22:20 +0200

    vim: add other mappings for gitsigns' hunk navigation as comments

--- nvim/.config/nvim/lua/mrv/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/mrv/plugins/gitsigns.lua
@@ -2,15 +2,20 @@ require('gitsigns').setup {
   keymaps = {
     noremap = true,
     buffer = true,
+    ['n <Leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['v <Leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <Leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <Leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['v <Leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <Leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <Leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
     ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk({wrap=false})<CR>'"},
     ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk({wrap=false})<CR>'"},
-    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+    -- Try out these other mappings
+    -- ['n <Leader>j'] = { expr = true, "&diff ? '<Leader>j' : '<cmd>lua require\"gitsigns.actions\".next_hunk({wrap=false})<CR>'"},
+    -- ['n <Leader>k'] = { expr = true, "&diff ? '<Leader>k' : '<cmd>lua require\"gitsigns.actions\".prev_hunk({wrap=false})<CR>'"},
+    -- ['n <M-]>'] = { expr = true, "&diff ? '<M-]>' : '<cmd>lua require\"gitsigns.actions\".next_hunk({wrap=false})<CR>'"},
+    -- ['n <M-[>'] = { expr = true, "&diff ? '<M-[>' : '<cmd>lua require\"gitsigns.actions\".prev_hunk({wrap=false})<CR>'"},
   },
 }
 
