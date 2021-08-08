935bfd43c309e029b9e2d3debe925732b834901a
Martin Roa Villescas <mroavi@gmail.com>
2021-08-08 20:20:09 +0200

    vim: disable <C-v> mapping in lf
    It conflicts with vimv when tyring to batch rename files

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -3,7 +3,8 @@ vim.g["lf#replace_netrw"] = true
 vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
 vim.g["lf#action"] = { ['<C-t>'] = 'tab split',
                        ['<C-x>'] = 'split',
-                       ['<C-v>'] = 'vsplit', }
+                       -- ['<C-v>'] = 'vsplit', -- conflicts with blockwise visual mode (needed for batch rename)
+                     }
 
 local utils = require("mrv.utils")
 utils.remap("n", "<Leader>/", ":LfPicker %:p:h<CR>")
