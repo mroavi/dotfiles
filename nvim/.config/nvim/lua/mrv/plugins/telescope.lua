1da0e63c06d272e6fc3bdb1508aefd71381e5c84
Martin Roa Villescas <mroavi@gmail.com>
2021-08-09 22:44:01 +0200

    vim: configure buffers to look like tmux's choose tree menu

--- nvim/.config/nvim/lua/mrv/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mrv/plugins/telescope.lua
@@ -58,11 +58,36 @@ end
 ---------------------------------------------------------------------------------
 
 function M.buffers()
-	require("telescope.builtin").buffers{
-		path_display = {
-			"tail",
-		},
-	}
+  require("telescope.builtin").buffers {
+    layout_strategy = "vertical",
+    layout_config = {mirror = true},
+    sorting_strategy = "ascending",
+    scroll_strategy = "limit",
+    path_display = {"tail"}, -- TODO: change to "smart" when merged: https://github.com/caojoshua/telescope.nvim/pull/1
+    attach_mappings = function(_, map)
+      map('i', 'k', actions.move_selection_previous)
+      map('i', 'j', actions.move_selection_next)
+      map('i', 'x', actions.delete_buffer)
+      return true
+    end,
+    on_complete = {
+      function(picker)
+        local buf = vim.api.nvim_win_get_buf(picker.original_win_id)
+        local selection_index
+        local idx = 1
+        for entry in picker.manager:iter() do
+          if entry.bufnr == buf then
+            selection_index = idx
+            break
+          end
+          idx = idx + 1
+        end
+        local row = picker:get_row(selection_index)
+        picker:set_selection(row)
+        picker._completion_callbacks = {}
+      end,
+    },
+  }
 end
 
 function M.lines()
@@ -134,6 +159,7 @@ end
 local utils = require('mrv.utils')
 
 -- File pickers
+utils.remap("n", "<Leader>'", "<Cmd>lua require('mrv.plugins.telescope').buffers()<CR>")
 utils.remap("n", "<Leader>fi", "<Cmd>lua require('mrv.plugins.telescope').find_files()<CR>")
 utils.remap("n", "<Leader>fg", "<Cmd>lua require('mrv.plugins.telescope').git_files()<CR>")
 utils.remap("n", "<Leader>rg", "<Cmd>lua require('telescope.builtin').live_grep()<CR>")
