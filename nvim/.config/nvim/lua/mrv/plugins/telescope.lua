55211d7a8c0e143555645904de5bef3db59b6625
Martin Roa Villescas <mroavi@gmail.com>
2021-08-10 02:05:06 +0200

    vim: split telescope's result list and preview vertically

--- nvim/.config/nvim/lua/mrv/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mrv/plugins/telescope.lua
@@ -9,6 +9,7 @@ local M = {}
 require("telescope").setup{
 	defaults = {
 		sorting_strategy = "descending",
+    layout_strategy = "vertical",
 		file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
 		grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
 		qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
