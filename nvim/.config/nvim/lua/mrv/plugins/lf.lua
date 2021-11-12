0856545a71bfd5fcf10115319538e1e3005b5b03
Martin Roa Villescas <mroavi@gmail.com>
2021-11-12 20:30:23 +0100

    vim: add action to lf to add file to arglist

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -3,6 +3,7 @@ vim.g["lf#replace_netrw"] = true
 vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
 vim.g["lf#action"] = { ['<C-t>'] = 'tab split',
                        ['<C-x>'] = 'split',
+                       ['<C-a>'] = 'arga', -- add to arglist
                        -- ['<C-v>'] = 'vsplit', -- conflicts with blockwise visual mode (needed for batch rename)
                      }
 
