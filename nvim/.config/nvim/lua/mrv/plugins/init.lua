6ca03946d19da14d071816e98975a64f327f8ff9
Martin Roa Villescas <mroavi@gmail.com>
2021-11-13 18:51:47 +0100

    vim: add distant plugin to the try out list

--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -59,6 +59,7 @@ M.setup = function()
     --use {'nvim-treesitter/nvim-treesitter', branch = '0.5-compat', run = ':TSUpdate'}
     --use {'terrortylor/nvim-comment', config = function() require('mrv.plugins.nvim-comment') end} -- a comment toggler for Neovim, written in Lua
     --use {'rafamadriz/friendly-snippets', } -- set of preconfigured snippets for different languages
+    --use {'chipsenkbeil/distant.nvim', } -- edit files, run programs, and work with LSP on a remote machine from the comfort of your local environment construction
 
     -- Abandoned
     --use {'raimondi/delimitmate', config = function() require('mrv.plugins.delimitmate') end} -- provides insert mode auto-completion for quotes, parens, brackets, etc.
