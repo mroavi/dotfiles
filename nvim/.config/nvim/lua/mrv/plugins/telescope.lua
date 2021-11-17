eefcfbf52ab605d169b47dc3fb6221cf4bf416dd
Martin Roa Villescas <mroavi@gmail.com>
2021-11-17 01:00:33 +0100

    vim: add help tags Telescope picker

--- nvim/.config/nvim/lua/mrv/plugins/telescope.lua
+++ nvim/.config/nvim/lua/mrv/plugins/telescope.lua
@@ -338,6 +338,7 @@ utils.remap("n", "<Leader>r", "<Cmd>lua require('mrv.plugins.telescope').recent_
 utils.remap("n", "<Leader>ch", "<Cmd>lua require('telescope.builtin').command_history()<CR>")
 --utils.remap("n", "<Leader>bl", "<Cmd>lua require('mrv.plugins.telescope').lines()<CR>")
 utils.remap("n", "<Leader>'", "<Cmd>lua require('mrv.plugins.telescope').marks()<CR>")
+utils.remap("n", "<Leader>he", "<Cmd>lua require('telescope.builtin').help_tags()<CR>")
 --utils.remap("n", "<Leader>ma", "<Cmd>lua require('telescope.builtin').keymaps()<CR>")
 -- LSP pickers
 --utils.remap("n", "<Leader>ds", "<Cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
