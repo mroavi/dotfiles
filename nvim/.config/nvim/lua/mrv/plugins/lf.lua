7529dc53742f035e58da15f6d313c09696d3a203
Martin Roa Villescas <mroavi@gmail.com>
2021-12-16 17:48:05 +0100

    Update action mappings to open splits in lf

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -1,10 +1,10 @@
 vim.g["lf#set_default_mappings"] = false
 vim.g["lf#replace_netrw"] = true
 vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
-vim.g["lf#action"] = { ['<C-t>'] = 'tab split',
-                       ['<C-x>'] = 'split',
+vim.g["lf#action"] = { ['<C-t>'] = 'tab split', -- open in tab
+                       ['<C-j>'] = 'split', -- horizontal split
                        ['<C-a>'] = 'arga', -- add to arglist
-                       --['<C-v>'] = 'vsplit', -- conflicts with blockwise visual mode (needed for batch rename)
+                       ['<C-l>'] = 'vsplit', -- vertical split
                      }
 
 local utils = require("mrv.utils")
