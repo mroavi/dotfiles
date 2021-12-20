a30161f913398938d7f90ab93591a00d1d3ddabb
Martin Roa Villescas <mroavi@gmail.com>
2021-12-20 22:54:23 +0100

    vim: disable marks in which-key

--- nvim/.config/nvim/lua/mrv/plugins/which-key.lua
+++ nvim/.config/nvim/lua/mrv/plugins/which-key.lua
@@ -1,6 +1,6 @@
 require("which-key").setup {
   plugins = {
-    marks = true, -- shows a list of your marks on ' and `
+    marks = false, -- shows a list of your marks on ' and `
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
     spelling = {
       enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
