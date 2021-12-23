f3ffe6ad98fd6c6570052b05e057e14888b2d344
Martin Roa Villescas <mroavi@gmail.com>
2021-12-23 14:33:47 +0100

    vim: remove compe configuration from autoparis config

--- nvim/.config/nvim/lua/mrv/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/mrv/plugins/autopairs.lua
@@ -2,7 +2,3 @@ require('nvim-autopairs').setup({
   disable_filetype = { "TelescopePrompt" , "vim" },
 })
 
-require("nvim-autopairs.completion.compe").setup({
-  map_cr = true, --  map <CR> on insert mode
-})
-
