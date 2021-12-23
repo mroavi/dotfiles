74e30a8a17b12ac7f6449ca4dc0d3e9b495efbf4
Martin Roa Villescas <mroavi@gmail.com>
2021-12-23 13:28:39 +0100

    lsp: minor formatting

--- nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
@@ -17,10 +17,11 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = false,
     virtual_text = true,
     signs = true,
+    underline = false,
     update_in_insert = false,
+    severity_sort = false,
   }
 )
 
