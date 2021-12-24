a6d00eccb3604677fa14263a3e39b74aefa27bad
Martin Roa Villescas <mroavi@gmail.com>
2021-12-24 15:28:23 +0100

    vim: set JuliaFormatter options

--- nvim/.config/nvim/lua/mrv/plugins/JuliaFormatter.lua
+++ nvim/.config/nvim/lua/mrv/plugins/JuliaFormatter.lua
@@ -2,3 +2,11 @@ local utils = require('mrv.utils')
 utils.keymap_buff(0, "n", "<Leader>f", "<Cmd>JuliaFormatterFormat<CR>")
 utils.keymap_buff(0, "v", "<Leader>f", "<Cmd>JuliaFormatterFormat<CR>")
 
+vim.g["JuliaFormatter_options"] = {
+  indent = 2,
+  margin = 92,
+  always_for_in = false,
+  whitespace_typedefs = false,
+  whitespace_ops_in_indices = true,
+}
+
