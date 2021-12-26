c85d9329ea4fd38a513995eff7561a6399c6db8a
Martin Roa Villescas <mroavi@gmail.com>
2021-12-26 21:10:48 +0100

    awesome: add comment

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -570,7 +570,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- Autostart applications
 awful.spawn.with_shell("picom") -- enables transparency
 awful.spawn.with_shell("dropbox")
-awful.spawn.with_shell("nitrogen --restore")
+awful.spawn.with_shell("nitrogen --restore") -- restores background image
 
 -- Gaps between windows
 beautiful.useless_gap = 5
