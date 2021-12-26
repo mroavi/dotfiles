3e0f678cd252b232614c2bf86950056dfb4b90ba
Martin Roa Villescas <mroavi@gmail.com>
2021-12-26 20:25:20 +0100

    awesome: change theme's background focus color

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -14,7 +14,7 @@ local theme = {}
 theme.font          = "sans 8"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
+theme.bg_focus      = "#444444"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
