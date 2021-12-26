cf46ae0ad095de30a0360d2ef84dfc87f7ecd252
Martin Roa Villescas <mroavi@gmail.com>
2021-12-26 21:58:37 +0100

    awesome: improve formatting

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -238,16 +238,10 @@ globalkeys = gears.table.join(
     awful.key({ modkey,           }, "l", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
 
-    awful.key({ modkey,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-        end,
+    awful.key({ modkey,           }, "j", function () awful.client.focus.byidx( 1) end,
         {description = "focus next by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
+    awful.key({ modkey,           }, "k", function () awful.client.focus.byidx(-1) end,
         {description = "focus previous by index", group = "client"}
     ),
     --awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
