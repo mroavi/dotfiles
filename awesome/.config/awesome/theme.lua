baedef0892e0b0ec1165422fd4a9158fd2d95197
Martin Roa Villescas <mroavi@gmail.com>
2021-12-27 12:54:25 +0100

    awesome: update theme's fg normal and focus colors

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -23,8 +23,8 @@ theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
+theme.fg_normal     = "#ffffff"
+theme.fg_focus      = "#73cef4"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
