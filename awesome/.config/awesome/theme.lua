ef3a44777b7c1e251060d5f1f6726327ccd45d8d
Martin Roa Villescas <mroavi@gmail.com>
2021-12-27 02:13:00 +0100

    awesome: make theme's bg_focus transparent

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -13,8 +13,9 @@ local theme = {}
 
 theme.font          = "sans 8"
 
+-- "<color>" .. "opacity"
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#393939"
+theme.bg_focus      = "#393939" .. "00"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
