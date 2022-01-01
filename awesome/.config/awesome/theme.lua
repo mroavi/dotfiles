99a64f782ce47cc2345d4fe74a77e02063d1fcfa
Martin Roa Villescas <mroavi@gmail.com>
2022-01-01 19:22:05 +0100

    awesome: format comment

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -34,9 +34,7 @@ theme.border_normal = "#000000"
 theme.border_focus  = "#73cef4"
 theme.border_marked = "#a9a1e1"
 
--- There are other variable sets
--- overriding the default one when
--- defined, the sets are:
+-- There are other variable sets overriding the default one when defined, the sets are:
 -- taglist_[bg|fg]_[focus|urgent|occupied|empty|volatile]
 -- tasklist_[bg|fg]_[focus|urgent]
 -- titlebar_[bg|fg]_[normal|focus]
