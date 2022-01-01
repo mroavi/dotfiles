7b929333c1e6a353097d7c5e24f8b20158f8126a
Martin Roa Villescas <mroavi@gmail.com>
2022-01-01 20:08:32 +0100

    awesome: fix formatting

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -119,25 +119,26 @@ awful.layout.layouts = {
 --------------------------------------------------------------------------------
 --- Wibar
 --------------------------------------------------------------------------------
+
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
-  -- Create a wibox for each screen and add it
-  local taglist_buttons = gears.table.join(
-    awful.button({ }, 1, function(t) t:view_only() end),
-    awful.button({ modkey }, 1, function(t)
-                              if client.focus then
-                                  client.focus:move_to_tag(t)
-                              end
-                          end),
-    awful.button({ }, 3, awful.tag.viewtoggle),
-    awful.button({ modkey }, 3, function(t)
-                              if client.focus then
-                                  client.focus:toggle_tag(t)
-                              end
-                          end),
-    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
-    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end))
+-- Create a wibox for each screen and add it
+local taglist_buttons = gears.table.join(
+  awful.button({ }, 1, function(t) t:view_only() end),
+  awful.button({ modkey }, 1, function(t)
+                            if client.focus then
+                                client.focus:move_to_tag(t)
+                            end
+                        end),
+  awful.button({ }, 3, awful.tag.viewtoggle),
+  awful.button({ modkey }, 3, function(t)
+                            if client.focus then
+                                client.focus:toggle_tag(t)
+                            end
+                        end),
+  awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+  awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end))
 
 --local tasklist_buttons = gears.table.join(
 --    awful.button({ }, 1, function (c)
