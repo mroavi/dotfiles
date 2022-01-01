f1431ae828722d941886058736d23233607df4bc
Martin Roa Villescas <mroavi@gmail.com>
2022-01-01 19:27:37 +0100

    awesome: update the theme's bg_urgent color

--- awesome/.config/awesome/theme.lua
+++ awesome/.config/awesome/theme.lua
@@ -19,7 +19,7 @@ theme.hotkeys_description_font  = font
 -- "<color>" .. "opacity"
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#393939" .. "00"
-theme.bg_urgent     = "#ff0000"
+theme.bg_urgent     = "#f43753"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
