acc6540e45baccc90b66d206687278941ad91fd6
Martin Roa Villescas <mroavi@gmail.com>
2022-01-06 13:22:29 +0100

    vim: remove vimtex deprecated option

--- nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
@@ -6,7 +6,6 @@ vim.g.vimtex_compiler_latexmk = {
 -- Configure Okular
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
-vim.g.vimtex_view_general_options_latexmk = "--unique"
 
 --vim.g.vimtex_quickfix_ignore_filters = { "Underfull .*", } -- disable custom warnings based on regexp
 
