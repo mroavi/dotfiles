fab5a36ceaa864cecdd97d1118117335f1c0331e
Martin Roa Villescas <mroavi@gmail.com>
2022-05-08 21:10:45 +0200

    vim: add gh.nvim to list of "try these out" plugins

--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -60,6 +60,7 @@ M.setup = function()
     --use {'rcarriga/nvim-notify', } -- a fancy, configurable, notification manager for NeoVim
     --use {'sainnhe/everforest', config = function() vim.cmd [[ let g:everforest_background = 'hard' | colorscheme everforest ]] end} -- comfortable & pleasant color scheme for vim
     --use {'lukas-reineke/indent-blankline.nvim'} -- indent guides for Neovim
+    --use {'ldelossa/gh.nvim.git'} -- Github integration powered by litee.nvim
 
     -- Abandoned
     --use {'raimondi/delimitmate', config = function() require('mrv.plugins.delimitmate') end} -- provides insert mode auto-completion for quotes, parens, brackets, etc.
