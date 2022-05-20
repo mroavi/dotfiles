f7429242297d731b65775daacdf9f27c3c6cc5e0
Martin Roa Villescas <mroavi@gmail.com>
2022-05-20 17:46:05 +0200

    vim: disable julia's indentation option

--- nvim/.config/nvim/lua/mrv/plugins/julia-vim.lua
+++ nvim/.config/nvim/lua/mrv/plugins/julia-vim.lua
@@ -2,3 +2,4 @@ vim.g.latex_to_unicode_tab = "off"
 vim.g.latex_to_unicode_suggestions = 0
 vim.g.julia_indent_align_brackets = true
 vim.g.julia_indent_align_funcargs = true
+vim.g.julia_set_indentation = 0
