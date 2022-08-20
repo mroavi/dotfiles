989442280ebe044ace9fa436b3ed32cc8951c937
Martin Roa Villescas <mroavi@gmail.com>
2022-08-21 00:22:17 +0200

    vim: rename setting.lua to config.lua

--- nvim/.config/nvim/lua/mrv/init.lua
+++ nvim/.config/nvim/lua/mrv/init.lua
@@ -5,6 +5,6 @@
 --    | | | | | | |_ _| | |_| | (_| |
 --    |_|_| |_|_|\__(_)_|\__,_|\__,_|
 
-require('mrv.settings').setup()
+require('mrv.config').setup()
 require('mrv.plugins').setup()
 vim.cmd [[ exe "source " .. stdpath('config') .. "/lua/mrv/custom.vim" ]]
