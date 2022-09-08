5054d089bfb36c11abc74408c8ac2cbbe3d9acad
Martin Roa Villescas <mroavi@gmail.com>
2022-09-08 13:55:34 +0200

    vim (lspconfig): fix formatting

--- nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
@@ -74,8 +74,7 @@ lspconfig.pyright.setup {}
 require'lspconfig'.julials.setup {
   autostart = true,
   on_new_config = function(new_config, _)
-    local julia = vim.fn
-                      .expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+    local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
     if require'lspconfig'.util.path.is_file(julia) then
       new_config.cmd[1] = julia
     end
