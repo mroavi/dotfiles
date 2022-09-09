a5d2fba2c851fb36f3abf207180a82859b5055a2
Martin Roa Villescas <mroavi@gmail.com>
2022-09-09 11:11:34 +0200

    vim (lf): reorder actions and update comments

--- nvim/.config/nvim/lua/mrv/plugins/lf.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf.lua
@@ -2,9 +2,9 @@ vim.g["lf#set_default_mappings"] = false
 vim.g["lf#replace_netrw"] = true
 vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
 vim.g["lf#action"] = { ['<C-t>'] = 'tab split', -- open in tab
-                       ['<C-j>'] = 'split', -- horizontal split
+                       ['<C-j>'] = 'split', -- open in horizontal split
+                       ['<C-l>'] = 'vsplit', -- open in vertical split
                        ['<C-a>'] = '$arga', -- add to arglist
-                       ['<C-l>'] = 'vsplit', -- vertical split
                      }
 
 vim.keymap.set("n", "<Leader>/",
