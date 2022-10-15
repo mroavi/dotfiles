32ab4a0e78fe9ce2dc4c2e35c30c8f4f7447e860
Martin Roa Villescas <mroavi@gmail.com>
2022-10-16 00:21:19 +0200

    vim: add config file for lmburns' lf plugin

---
+++ nvim/.config/nvim/lua/mrv/plugins/lf-nvim.lua
@@ -0,0 +1,20 @@
+-- This feature will not work if the plugin is lazy-loaded
+vim.g.lf_netrw = 1
+
+require("lf").setup({
+  default_actions = {
+    ["<C-t>"] = "tabedit",
+    ["<C-j>"] = "split",
+    ["<C-l>"] = "vsplit",
+    ['<C-a>'] = '$arga', -- add to end of arglist
+  },
+  winblend = 0, -- psuedotransparency level
+  border = "rounded", -- border kind: single double shadow curved
+  escape_quit = false, -- map escape to the quit command (so it doesn't go into a meta normal mode)
+  highlights = { -- highlights passed to toggleterm
+    NormalFloat = {guifg = require("tender.colors").green},
+    FloatBorder = {guifg = require("tender.colors").white},
+  },
+})
+
+vim.keymap.set("n", "<Leader>/", ":Lf<CR>")
