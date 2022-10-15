6dce444b2c3d01605d7e52a04fdc8557d8b60b8b
Martin Roa Villescas <mroavi@gmail.com>
2022-10-16 00:23:05 +0200

    Add a config file for the toggleterm plugin

---
+++ nvim/.config/nvim/lua/mrv/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+require("toggleterm").setup{
+  open_mapping = [[<Leader>>]],
+  insert_mappings = false, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = false, -- whether or not the open mapping applies in the opened terminals
+  direction = 'float',
+  --on_open = function(term)
+  --  vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<Esc>", "<C-d>", { noremap = true, silent = true })
+  --end,
+}
