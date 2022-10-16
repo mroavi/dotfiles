95ec1d89c1eaf8167eaf4a520b9b3bd52e0aac10
Martin Roa Villescas <mroavi@gmail.com>
2022-10-16 07:58:39 +0200

    vim: fix keymap.set for Neovim 0.8
    by adding `replace_keycodes = false` to opts

--- nvim/.config/nvim/lua/mrv/plugins/lf-vim.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf-vim.lua
@@ -1,17 +1,15 @@
 vim.g["lf#set_default_mappings"] = false
 vim.g["lf#replace_netrw"] = true
-vim.g["lf#layout"] = { window = { width = 0.9, height = 0.9, highlight = "Normal", } }
-vim.g["lf#action"] = { ['<C-t>'] = 'tab split', -- open in tab
-                       ['<C-j>'] = 'split', -- open in horizontal split
-                       ['<C-l>'] = 'vsplit', -- open in vertical split
-                       ['<C-a>'] = '$arga', -- add to arglist
-                     }
+vim.g["lf#layout"] = {
+  window = {width = 0.9, height = 0.9, highlight = "Normal"}
+}
+vim.g["lf#action"] = {
+  ['<C-t>'] = 'tab split', -- open in tab
+  ['<C-j>'] = 'split', -- open in horizontal split
+  ['<C-l>'] = 'vsplit', -- open in vertical split
+  ['<C-a>'] = '$arga' -- add to arglist
+}
 
--- TODO: stopped working with Neovim 0.8
---vim.keymap.set("n", "<Leader>/",
---               "expand('%') == '' ? '<Cmd>Lp %:p:h<CR>' : '<Cmd>Lp %<CR>'",
---               {noremap = true, expr = true})
-
-vim.cmd [[
-nnoremap <expr> <Leader>/ expand('%') == '' ? '<Cmd>Lp %:p:h<CR>' : '<Cmd>Lp %<CR>'
-]]
+vim.keymap.set("n", "<Leader>/",
+               "expand('%') == '' ? '<Cmd>Lp %:p:h<CR>' : '<Cmd>Lp %<CR>'",
+               {noremap = true, expr = true, replace_keycodes = false})
