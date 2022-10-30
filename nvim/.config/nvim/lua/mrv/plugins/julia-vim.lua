95e0822166c24fb6f815cdf0042b90c447801b46
Martin Roa Villescas <mroavi@gmail.com>
2022-10-30 15:19:04 +0100

    vim (julia-vim): define extra customization options

--- nvim/.config/nvim/lua/mrv/plugins/julia-vim.lua
+++ nvim/.config/nvim/lua/mrv/plugins/julia-vim.lua
@@ -1,5 +1,12 @@
 vim.g.latex_to_unicode_tab = "off"
-vim.g.latex_to_unicode_suggestions = 0
+vim.g.latex_to_unicode_suggestions = false
+
+-- Customization
+vim.g.julia_spellcheck_docstrings = true
+vim.g.julia_spellcheck_strings = false
+vim.g.julia_spellcheck_comments = true
+vim.g.julia_highlight_operators = true
+vim.g.julia_indent_align_import = true
 vim.g.julia_indent_align_brackets = true
-vim.g.julia_indent_align_funcargs = true
-vim.g.julia_set_indentation = 0
+vim.g.julia_indent_align_funcargs = false
+vim.g.julia_set_indentation = false
