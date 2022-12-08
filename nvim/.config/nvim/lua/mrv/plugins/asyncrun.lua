380da47ae001b99d0a8839a3b3a585dbae657866
Martin Roa Villescas <mroavi@gmail.com>
2022-12-08 21:30:38 +0100

    vim (AsyncRun): implement visual start search

--- nvim/.config/nvim/lua/mrv/plugins/asyncrun.lua
+++ nvim/.config/nvim/lua/mrv/plugins/asyncrun.lua
@@ -1,2 +1,14 @@
-vim.g.asyncrun_open = 10
-vim.keymap.set("n", "<Leader>*", ":AsyncRun! -strip grep -R -n <cword><Cr>", { silent = false })
+vim.g.asyncrun_open = 10 -- open quickfix window at given height after command starts
+
+-- TODO: use ripgrep instead of grep and factor out the command options
+
+-- Borrowed tricks from: https://learnvimscriptthehardway.stevelosh.com/chapters/32.html
+--vim.keymap.set("n", "<Leader>*", [[:exe "AsyncRun! -strip grep -R -n " . shellescape(expand("<cword>")) . " ."<Cr>]], { silent = false }) -- equivalent to the line below
+vim.keymap.set("n", "<Leader>*", "':AsyncRun! -strip grep -R -n -F ' . shellescape(expand('<cword>')) . ' .<Cr>'", { silent = false, expr = true })
+
+-- Passes the `-F` flag which makes grep interpret text as fixed strings, not regular expressions
+local function visual_star_grep()
+  local visual_selection = require('mrv.utils').get_visual_selection()
+  vim.cmd("AsyncRun! -strip grep -R -n -F " .. vim.fn.shellescape(visual_selection) .. " .")
+end
+vim.keymap.set('x', '<Leader>*', function() visual_star_grep() end)
