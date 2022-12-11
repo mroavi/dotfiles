9fa9ca17bf01780e8a80ed28862213dab85d35ea
Martin Roa Villescas <mroavi@gmail.com>
2022-12-11 09:00:31 +0100

    vim (slides): add function to refresh status line on each slide change

--- nvim/.config/nvim/lua/mrv/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/mrv/plugins/zen-mode.lua
@@ -1,3 +1,27 @@
+local function cmdline_clear()
+  local key = vim.api.nvim_replace_termcodes("<C-l>", true, false, true)
+  vim.api.nvim_feedkeys(key, 'n', false) -- clear command line
+end
+
+-- TODO: zen-mode doesn't provide a way to display the status line
+local function status_line_refresh()
+  local conceal_color = "%#Conceal#" -- set `Conceal` highlight color
+  local date = "%{StatuslineDate()}" -- insert date
+  local right_align = "%=" -- move to the right side of the status line
+  local slide_number = "%{StatuslineSlideNumber()}" -- insert slide number
+  return string.format(
+    "%s%s%s%s",
+    conceal_color,
+    date,
+    right_align,
+    slide_number
+  )
+end
+
+local function status_line_reset()
+  return [[%f %h%w%m%r %=%(%l,%c%V %= %P%)]]
+end
+
 local opts = {
   window = {
     backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
@@ -41,9 +65,12 @@ local opts = {
   },
   -- callback where you can add custom code when the Zen window opens
   on_open = function(win)
+    cmdline_clear()
+    vim.opt.statusline = status_line_refresh()
   end,
   -- callback where you can add custom code when the Zen window closes
   on_close = function()
+    vim.opt.statusline = status_line_reset()
   end,
 }
 
