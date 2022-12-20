59cbc6560643fe4be8004ae230ebd2a9ac5cdfa8
Martin Roa Villescas <mroavi@gmail.com>
2022-12-20 01:15:50 +0100

    vim (cmp and luasnip): move mappings to proper place

--- nvim/.config/nvim/lua/mrv/plugins/cmp.lua
+++ nvim/.config/nvim/lua/mrv/plugins/cmp.lua
@@ -88,16 +88,3 @@ cmp.setup {
     { name = "buffer", keyword_length = 3 },
   },
 }
-
-vim.cmd [[
-
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-
-imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-
-]]
--- nvim/.config/nvim/lua/mrv/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/mrv/plugins/luasnip.lua
@@ -1,1 +1,28 @@
-require("luasnip/loaders/from_vscode").lazy_load()
+local ls = require "luasnip"
+local vim = vim
+
+--require("luasnip/loaders/from_vscode").lazy_load() -- to use existing vs-code style snippets from a plugin
+
+-- Simple example of a global snippet (type "expand" followed by <Tab> )
+ls.add_snippets("all", {
+    -- Available for any filetype
+    ls.parser.parse_snippet("expand", "Hello, World!"),
+  }
+)
+
+-- Expand or jump forward
+vim.keymap.set({ "i", "s" }, "<Tab>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  else
+    local key = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+    vim.api.nvim_feedkeys(key, 'n', false)
+  end
+end, { silent = true })
+
+-- Jump backwards
+vim.keymap.set({ "i", "s" }, "<S-Tab>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true })
