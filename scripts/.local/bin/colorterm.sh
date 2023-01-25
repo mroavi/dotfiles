a28e206d2c4c3f62b65ea161a43ce07949e33bef
Martin Roa Villescas <mroavi@gmail.com>
2023-01-25 16:45:47 +0100

    colorterm: add `hpc` to set of supported hosts

--- scripts/.local/bin/colorterm.sh
+++ scripts/.local/bin/colorterm.sh
@@ -12,7 +12,7 @@ fi
 # Otherwise, set the color based on the hostname
 if [ "$1" == "co23" ]; then
   printf '\033]11;#192436\007'
-elif [ "$1" == "co3" ]; then
+elif [ "$1" == "hpc" ]; then
   printf '\033]11;#253320\007'
 else
   # Other hostname
