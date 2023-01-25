1aefaf25947b746b050897e02ee6662f4d338660
Martin Roa Villescas <mroavi@gmail.com>
2023-01-25 18:18:12 +0100

    scripts (colorterm): update host colors

--- scripts/.local/bin/colorterm.sh
+++ scripts/.local/bin/colorterm.sh
@@ -11,9 +11,9 @@ fi
 
 # Otherwise, set the color based on the hostname
 if [ "$1" == "co23" ]; then
-  printf '\033]11;#192436\007'
-elif [ "$1" == "hpc" ]; then
   printf '\033]11;#253320\007'
+elif [ "$1" == "hpc" ]; then
+  printf '\033]11;#192436\007'
 else
   # Other hostname
   printf '\033]11;#331C1F\007'
