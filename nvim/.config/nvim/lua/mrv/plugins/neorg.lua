177a720dc774de0ea67242f1e6053e3f5c4c235c
Martin Roa Villescas <mroavi@gmail.com>
2023-02-23 12:01:38 +0100

    vim (neorg): update config

--- nvim/.config/nvim/lua/mrv/plugins/neorg.lua
+++ nvim/.config/nvim/lua/mrv/plugins/neorg.lua
@@ -1,12 +1,17 @@
 require('neorg').setup {
   load = {
     ["core.defaults"] = {}, -- loads default behaviour
-    ["core.norg.concealer"] = {}, -- adds pretty icons to your documents
+    ["core.norg.concealer"] = { -- adds pretty icons to your documents
+      config = {
+        icon_preset = "diamond", -- other options are "basic" and "varied"
+      }
+    },
     ["core.norg.dirman"] = { -- manages Neorg workspaces
       config = {
         workspaces = {
           notes = "~/notes",
         },
+        default_workspace = "notes",
       },
     },
   },
