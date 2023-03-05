7fe006cd5f7f42fb21d66d178fcec9320d2361f2
Martin Roa Villescas <mroavi@gmail.com>
2023-03-05 21:01:32 +0100

    vim (lf): update highlights

--- nvim/.config/nvim/lua/mrv/plugins/lf-nvim.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lf-nvim.lua
@@ -12,8 +12,12 @@ require("lf").setup({
   border = "rounded", -- border kind: single double shadow curved
   escape_quit = false, -- map escape to the quit command (so it doesn't go into a meta normal mode)
   highlights = { -- highlights passed to toggleterm
-    --NormalFloat = { guifg = require("tender.colors").white },
-    --FloatBorder = { guifg = require("tender.colors").white },
+    Normal = { link = 'Normal' },
+    NormalFloat = { link = 'Normal' },
+    FloatBorder = { link = 'FloatBorder' },
+    SignColumn = { link = 'SignColumn' },
+    StatusLine = { link = 'StatusLine' },
+    StatusLineNC = { link = 'StatusLineNC' },
   },
 })
 
