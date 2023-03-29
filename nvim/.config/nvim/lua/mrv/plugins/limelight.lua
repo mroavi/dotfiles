2ada7407e69fe842ad192f1a78a750f31fa291a8
Martin Roa Villescas <mroavi@gmail.com>
2023-03-29 14:39:56 +0200

    vim (limelight): add custom beginning/end of paragraph defs

--- nvim/.config/nvim/lua/mrv/plugins/limelight.lua
+++ nvim/.config/nvim/lua/mrv/plugins/limelight.lua
@@ -5,3 +5,10 @@ vim.g.limelight_conceal_ctermfg = 240
 -- Color name (:help gui-colors) or RGB color
 vim.g.limelight_conceal_guifg = 'DarkGray'
 vim.g.limelight_conceal_guifg = '#777777'
+
+-- " Custom beginning/end of paragraph (only highlights one paragraph at any given time)
+-- `zs` and `ze` are explained in: https://vim.fandom.com/wiki/Regex_lookahead_and_lookbehind
+vim.cmd([[
+let g:limelight_bop = '^\s*$\n'
+let g:limelight_eop = '^\s*$'
+]])
