ee432075baf8025b2fb51ec202235f2f3270a1a3
Martin Roa Villescas <mroavi@gmail.com>
2023-04-30 09:59:05 +0200

    vim (neorg): fix errors due to breaking changes

--- nvim/.config/nvim/lua/mrv/plugins/neorg.lua
+++ nvim/.config/nvim/lua/mrv/plugins/neorg.lua
@@ -1,12 +1,12 @@
 require('neorg').setup {
   load = {
     ["core.defaults"] = {}, -- loads default behaviour
-    ["core.norg.concealer"] = { -- adds pretty icons to your documents
+    ["core.concealer"] = { -- adds pretty icons to your documents
       config = {
         icon_preset = "diamond", -- other options are "basic" and "varied"
       }
     },
-    ["core.norg.dirman"] = { -- manages Neorg workspaces
+    ["core.dirman"] = { -- manages Neorg workspaces
       config = {
         workspaces = {
           notes = "~/notes",
