edf8ae9a399f4b98de4c73c9d7a19165060c89e6
Martin Roa Villescas <mroavi@gmail.com>
2023-05-16 10:04:53 +0200

    vim (vimtex): ignore underfull and overfull warnings

--- nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
@@ -7,4 +7,5 @@ vim.g.vimtex_compiler_latexmk = {
 vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 
---vim.g.vimtex_quickfix_ignore_filters = { "Underfull .*", } -- disable custom warnings based on regexp
+-- Disable custom warnings based on regexp
+vim.g.vimtex_quickfix_ignore_filters = { "Underfull .*", "Overfull"}
