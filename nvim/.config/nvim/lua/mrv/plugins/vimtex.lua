4d44e16f125e49d10e2d11be91404ac262844bde
Martin Roa Villescas <mroavi@gmail.com>
2023-08-24 14:57:43 +0200

    vim (vimtex): update quickfix ignore filters

--- nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/mrv/plugins/vimtex.lua
@@ -9,4 +9,9 @@ vim.g.vimtex_view_general_viewer = "okular"
 vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 
 -- Disable custom warnings based on regexp
-vim.g.vimtex_quickfix_ignore_filters = { "Underfull .*", "Overfull"}
+vim.g.vimtex_quickfix_ignore_filters = {
+  "Underfull",
+  "Overfull",
+  "Missing", -- TEMP
+  "Unknown document class", -- TEMP
+}
