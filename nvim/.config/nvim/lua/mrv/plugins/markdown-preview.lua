bcf1f8d727635702cf28b337f64e627a7b9a58ca
Martin Roa Villescas <mroavi@gmail.com>
2023-08-30 12:04:21 +0200

    vim (markdown-preview): remove old todo comment

--- nvim/.config/nvim/lua/mrv/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/mrv/plugins/markdown-preview.lua
@@ -1,18 +1,1 @@
 vim.keymap.set("n", "<Leader>md", "<Cmd>MarkdownPreview<CR>")
-
---let g:mkdp_preview_options = {
---      \ 'mkit': {},
---      \ 'katex': {},
---      \ 'uml': {},
---      \ 'maid': {},
---      \ 'disable_sync_scroll': 0,
---      \ 'sync_scroll_type': 'middle',
---      \ 'hide_yaml_meta': 1,
---      \ 'sequence_diagrams': {},
---      \ 'flowchart_diagrams': {},
---      \ 'content_editable': v:false,
---      \ 'disable_filename': 1
---      \ }
-
--- TODO: port the above
---vim.g["mkdp_preview_options"] = {}
