61f3cb5a4950d4a475e4797afc63d204d8898c8d
Martin Roa Villescas <mroavi@gmail.com>
2023-09-03 06:14:53 +0200

    vim (mini-splitjoin): update config

--- nvim/.config/nvim/lua/mrv/plugins/mini-splitjoin.lua
+++ nvim/.config/nvim/lua/mrv/plugins/mini-splitjoin.lua
@@ -1,4 +1,4 @@
-local options = {
+require('mini.splitjoin').setup({
   -- Module mappings. Use `''` (empty string) to disable one.
   -- Created for both Normal and Visual modes.
   mappings = {
@@ -6,33 +6,4 @@ local options = {
     split = '',
     join = '',
   },
-
-  -- Detection options: where split/join should be done
-  detect = {
-    -- Array of Lua patterns to detect region with arguments.
-    -- Default: { '%b()', '%b[]', '%b{}' }
-    brackets = nil,
-
-    -- String Lua pattern defining argument separator
-    separator = ',',
-
-    -- Array of Lua patterns for sub-regions to exclude separators from.
-    -- Enables correct detection in presence of nested brackets and quotes.
-    -- Default: { '%b()', '%b[]', '%b{}', '%b""', "%b''" }
-    exclude_regions = nil,
-  },
-
-  -- Split options
-  split = {
-    hooks_pre = {},
-    hooks_post = {},
-  },
-
-  -- Join options
-  join = {
-    hooks_pre = {},
-    hooks_post = {},
-  },
-}
-
-require('mini.splitjoin').setup(options)
+})
