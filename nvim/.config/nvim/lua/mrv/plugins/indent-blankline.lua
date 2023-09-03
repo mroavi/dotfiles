db0e11b3072dea9bf8264a19614e96a406cf0f6a
Martin Roa Villescas <mroavi@gmail.com>
2023-09-03 14:26:51 +0200

    vim (indent_blankine): disable lazy loading with keys (throws error)

--- nvim/.config/nvim/lua/mrv/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/mrv/plugins/indent-blankline.lua
@@ -1,2 +1,4 @@
+require("indent_blankline").setup({})
 vim.cmd[[IndentBlanklineDisable!]]
+
 vim.keymap.set("n", "yog", ":IndentBlanklineToggle<CR>")
--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -37,7 +37,7 @@ local plugins = {
   { 'mroavi/vim-evanesco' }, -- automatically clears search highlight
   { "catppuccin/nvim", name = "catppuccin", config = function() require('mrv.plugins.catppuccin') end }, -- soothing pastel theme for (Neo)vim
   { 'junegunn/vim-easy-align', keys = { 'ga', {'ga', mode = 'x' } }, init = function() require('mrv.plugins.easy-align') end }, -- a Vim alignment plugin
-  { "lukas-reineke/indent-blankline.nvim", keys = 'yog', config = function() require('mrv.plugins.indent-blankline') end }, -- indent guides for Neovim
+  { "lukas-reineke/indent-blankline.nvim", config = function() require('mrv.plugins.indent-blankline') end }, -- indent guides for Neovim
   { 'junegunn/gv.vim', cmd = 'GV', init = function() require('mrv.plugins.gv') end }, -- a git commit browser in Vim
   { "echasnovski/mini.splitjoin", keys = 'gS', config = function() require('mrv.plugins.mini-splitjoin') end }, -- Neovim Lua plugin to split and join arguments. Part of 'mini.nvim' library.
 
