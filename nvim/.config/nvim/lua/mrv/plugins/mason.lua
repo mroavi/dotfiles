5ed16fc2a3b8f0ce71081347e8fea5145f616fb7
Martin Roa Villescas <mroavi@gmail.com>
2023-09-09 14:51:46 +0200

    vim (mason): do not try to install all LSP servers if in SSH connection

--- nvim/.config/nvim/lua/mrv/plugins/mason.lua
+++ nvim/.config/nvim/lua/mrv/plugins/mason.lua
@@ -9,20 +9,41 @@ require("mason").setup({
   }
 })
 
-require("mason-lspconfig").setup({
-  -- For a list of all available packages, see: https://mason-registry.dev/registry/list
-  ensure_installed = {
-    "lua_ls",
-    "vimls",
-    "pyright",
-    "bashls",
-    "clangd",
-    "texlab",
-    "rust_analyzer",
-    "efm",
-    "arduino_language_server",
-    "cmake",
+
+if not vim.env.SSH_CONNECTION then
+  local opts = {
+    -- For a list of all available packages, see: https://mason-registry.dev/registry/list
+    ensure_installed = {
+      "lua_ls",
+      "vimls",
+      "pyright",
+      "bashls",
+      "clangd",
+      "texlab",
+      "rust_analyzer",
+      "efm",
+      "arduino_language_server",
+      "cmake",
+    }
   }
-})
+else
+  local opts = {
+    -- For a list of all available packages, see: https://mason-registry.dev/registry/list
+    ensure_installed = {
+      "lua_ls",
+      --"vimls",
+      --"pyright",
+      --"bashls",
+      --"clangd",
+      "texlab",
+      "rust_analyzer",
+      "efm",
+      "arduino_language_server",
+      "cmake",
+    }
+  }
+end
+
+require("mason-lspconfig").setup(opts)
 
 vim.keymap.set("n", "<Leader>M", ":Mason<CR>")
