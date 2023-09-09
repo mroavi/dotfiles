2ba2cfd5eecff6c077797f5a8fe284f2bd39806b
Martin Roa Villescas <mroavi@gmail.com>
2023-09-09 16:52:02 +0200

    vim (lsp): enable LUA lsp in SSH connection

--- nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
@@ -83,36 +83,36 @@ require 'lspconfig'.julials.setup({
   capabilities = capabilities,
 })
 
-if not vim.env.SSH_CONNECTION then
+--------------------------------------------------------------------------------
+--- lua
+--------------------------------------------------------------------------------
 
-  --------------------------------------------------------------------------------
-  --- lua
-  --------------------------------------------------------------------------------
-
-  -- Manual installation: sudo pacman -S lua-language-server
-  require 'lspconfig'.lua_ls.setup {
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = { 'vim' },
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-          checkThirdParty = false,
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
+-- Manual installation: sudo pacman -S lua-language-server
+require 'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
-  }
+  },
+}
+
+if not vim.env.SSH_CONNECTION then
 
   --------------------------------------------------------------------------------
   --- vim
