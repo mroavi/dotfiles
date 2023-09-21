167e6971ab2ec86b05352e39881be3f550be07dd
Martin Roa Villescas <mroavi@gmail.com>
2023-09-21 18:22:34 +0200

    vim (html): add support for browser-sync and tomux

---
+++ nvim/.config/nvim/ftplugin/html.lua
@@ -0,0 +1,42 @@
+local vim = vim
+
+local M = {}
+
+---------------------------------------------------------------------------------
+-- vim-tomux
+---------------------------------------------------------------------------------
+
+-- Dependency installation:
+-- npm install -g browser-sync
+
+-- Default config
+vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
+
+vim.g.tomux_use_clipboard = 0
+
+-- Split the current pane vertically and start 'browser-sync'
+function M.open_right_of()
+  vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
+  vim.cmd[[TomuxCommand("split-window -h -d -c " . expand("%:p:h"))]]
+  vim.cmd[[TomuxSend("browser-sync start --server --files '*.html, *.css, *.js'\n")]]
+end
+vim.keymap.set('n', '<Leader>tl', function() M.open_right_of() end, { buffer = true })
+
+-- Split the current pane horizontally and start 'browser-sync'
+function M.open_down_of()
+  vim.b.tomux_config = { socket_name = "default", target_pane = "{down-of}" }
+  vim.cmd[[TomuxCommand("split-window -v -d -l 10% -c " . expand("%:p:h"))]]
+  vim.cmd[[TomuxSend("browser-sync start --server --files '*.html, *.css, *.js'\n")]]
+end
+vim.keymap.set('n', '<Leader>tj', function() M.open_down_of() end, { buffer = true })
+
+-- Kill pane
+function M.kill_pane()
+  vim.cmd[[TomuxCommand("kill-pane -t " . shellescape(b:tomux_config["target_pane"]))]]
+end
+vim.keymap.set('n', '<Leader>tk', function() M.kill_pane() end, { buffer = true })
+
+-- Clear pane
+vim.keymap.set('n', '<Leader>cl', function() vim.cmd[[TomuxSend("clr\n")]] end, { buffer = true })
+
+return M
