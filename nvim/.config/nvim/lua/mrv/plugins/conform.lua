5854d4313548d42a6af0a5a2769ffa9c7ea15337
Martin Roa Villescas <mroavi@gmail.com>
2024-02-06 10:14:05 +0100

    vim (conform): prepend -m to the latexindex command

--- nvim/.config/nvim/lua/mrv/plugins/conform.lua
+++ nvim/.config/nvim/lua/mrv/plugins/conform.lua
@@ -4,5 +4,12 @@ conform.setup({
   formatters_by_ft = {
     python = { "black" },
     tex = { "latexindent" },
+  },
+  -- Override/add to the default values of formatters
+  formatters = {
+    latexindent = {
+      -- Prepend the ``-m`` (modifylinebreaks) switch
+      prepend_args = { "-m" },
+    }
   }
 })
