b47f1277f8fda8a12d1c7055caa78107103c6cd3
Martin Roa Villescas <mroavi@gmail.com>
2024-02-21 19:34:50 +0100

    vim (html): refactor start browser_sync cmd and disable notification

--- nvim/.config/nvim/ftplugin/html.lua
+++ nvim/.config/nvim/ftplugin/html.lua
@@ -17,6 +17,9 @@ vim.keymap.set('n', "zfit", [[:norm vitkojzf<Cr>]], { buffer = true, silent = tr
 -- Dependency installation:
 -- npm install -g browser-sync
 
+-- Start browser-sync cmd
+vim.b.start_browser_sync_cmd = "browser-sync start --server --files '*.html, *.css, *.js' --no-notify"
+
 -- Default config
 vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
 
@@ -26,7 +29,7 @@ vim.g.tomux_use_clipboard = 0
 function M.open_right_of()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
   vim.cmd[[TomuxCommand("split-window -h -d -c " . expand("%:p:h"))]]
-  vim.cmd[[TomuxSend("browser-sync start --server --files '*.html, *.css, *.js'\n")]]
+  vim.cmd[[TomuxSend(b:start_browser_sync_cmd . "\n")]]
 end
 vim.keymap.set('n', '<Leader>tl', function() M.open_right_of() end, { buffer = true })
 
@@ -34,7 +37,7 @@ vim.keymap.set('n', '<Leader>tl', function() M.open_right_of() end, { buffer = t
 function M.open_down_of()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{down-of}" }
   vim.cmd[[TomuxCommand("split-window -v -d -l 10% -c " . expand("%:p:h"))]]
-  vim.cmd[[TomuxSend("browser-sync start --server --files '*.html, *.css, *.js'\n")]]
+  vim.cmd[[TomuxSend(b:start_browser_sync_cmd . "\n")]]
 end
 vim.keymap.set('n', '<Leader>tj', function() M.open_down_of() end, { buffer = true })
 
