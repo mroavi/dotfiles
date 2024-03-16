f14b3d6c358b125b37a7cdb3008f2674fd2c6d35
Martin Roa Villescas <mroavi@gmail.com>
2024-03-16 15:11:16 +0100

    vim (cs): start new panes in vim's current working directory

--- nvim/.config/nvim/ftplugin/cs.lua
+++ nvim/.config/nvim/ftplugin/cs.lua
@@ -39,16 +39,18 @@ vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
 vim.g.tomux_use_clipboard = 0
 
 -- Split the current window vertically, adding a new pane to the right
+-- The new pane starts in Vim's current working directory
 function M.open_right_of()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
-  vim.cmd[[TomuxCommand("split-window -h -d -c " . expand("%:p:h"))]]
+  vim.cmd[[TomuxCommand("split-window -h -d -c " . getcwd())]]
 end
 vim.keymap.set('n', '<Leader>tl', function() M.open_right_of() end, { buffer = true })
 
 -- Split the current window horizontally, adding a new pane to the bottom
+-- The new pane starts in Vim's current working directory
 function M.open_bottom_of()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{down-of}" }
-  vim.cmd[[TomuxCommand("split-window -v -d -l 20% -c " . expand("%:p:h"))]]
+  vim.cmd[[TomuxCommand("split-window -v -d -l 20% -c "  . getcwd())]]
 end
 vim.keymap.set('n', '<Leader>tj', function() M.open_bottom_of() end, { buffer = true })
 
