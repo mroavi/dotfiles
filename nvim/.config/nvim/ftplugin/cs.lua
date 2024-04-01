65da3956942c8b36e08ed562bbeddb25eaea77fc
Martin Roa Villescas <mroavi@gmail.com>
2024-04-01 17:35:10 +0200

    vim (cs): Use 4 spaces for indentation

--- nvim/.config/nvim/ftplugin/cs.lua
+++ nvim/.config/nvim/ftplugin/cs.lua
@@ -6,6 +6,11 @@ local M = {}
 -- Define cell_delimeter
 vim.b.cell_delimeter = '/// '
 
+-- Use 4 spaces for indentation
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
 ---------------------------------------------------------------------------------
 --- Debug utilities
 ---------------------------------------------------------------------------------
