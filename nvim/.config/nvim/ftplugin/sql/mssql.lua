d8a1673f9d8a29ef712e85903e821bf50f8487d4
Martin Roa Villescas <mroavi@gmail.com>
2024-04-16 17:00:13 +0200

    vim (sql): use dir-based ftplugin to config sql for mariadb or mssql

---
+++ nvim/.config/nvim/ftplugin/sql/mssql.lua
@@ -0,0 +1,48 @@
+local vim = vim
+local utils = require('mrv.utils')
+
+-- Define cell_delimeter
+vim.b.cell_delimeter = '-- @block'
+
+--------------------------------------------------------------------------------
+--- vim-commentary
+--------------------------------------------------------------------------------
+
+-- No space between comment characters and code
+vim.b.commentary_format = '--%s'
+
+---------------------------------------------------------------------------------
+-- vim-tomux
+---------------------------------------------------------------------------------
+
+-- Default config
+vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
+
+vim.g.tomux_use_clipboard = 0
+
+-- Start REPL cmd
+vim.b.start_repl_cmd = 'sqlcmd -C -S localhost -U sa'
+
+local M = {}
+
+-- Start mariadb command line in a RIGHT split with active buffer as CWD
+function M.open_right_of()
+  vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
+  vim.cmd[[TomuxCommand("split-window -h -d -c " . expand("%:p:h"))]]
+  vim.cmd[[TomuxSend(b:start_repl_cmd . "\n")]]
+end
+vim.keymap.set('n', '<Leader>tl', function() M.open_right_of() end, { buffer = true })
+
+-- Clear command line
+function M.clear()
+  vim.cmd[[TomuxCommand("send-keys -t " . shellescape(b:tomux_config["target_pane"]) . " C-l")]]
+end
+vim.keymap.set('n', '<Leader>cl', function() M.clear() end, { buffer = true })
+
+-- Kill pane
+function M.kill_pane()
+  vim.cmd[[TomuxCommand("kill-pane -t " . shellescape(b:tomux_config["target_pane"]))]]
+end
+vim.keymap.set('n', '<Leader>tk', function() M.kill_pane() end, { buffer = true })
+
+return M
