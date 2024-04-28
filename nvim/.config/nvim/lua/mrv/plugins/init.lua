e5f5ab24227cf906659defbb0f61e39cdd8fabb2
Martin Roa Villescas <mroavi@gmail.com>
2024-04-28 12:34:17 +0200

    vim (lazy): cleanup

--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -44,45 +44,32 @@ local plugins = {
   -- {{{1
 
   -- Disabled
-  --{ 'NvChad/nvim-colorizer.lua', config = function() require('colorizer').setup() end }, -- the fastest Neovim colorizer.
-  --{ 'preservim/vim-markdown' }, -- syntax highlighting, matching rules and mappings for Markdown and extensions
-  --{ 'dstein64/nvim-scrollview', config = function() require('mrv.plugins.scrollview') end }, -- a Neovim plugin that displays (non-interactive) scrollbars
-  --{ 'kdheepak/JuliaFormatter.vim', ft = "julia", config = function() require('mrv.plugins.JuliaFormatter') end }, -- formatter for Julia
+  --{ 'NvChad/nvim-colorizer.lua', config = function() require('colorizer').setup() end }, -- the fastest Neovim colorizer
   --{ 'folke/lua-dev.nvim' }, -- dev setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API
   --{ 'iamcco/markdown-preview.nvim', build = function() vim.fn['mkdp#util#install']() end, ft = { 'markdown' } }, -- preview markdown on your browser with synchronised scrolling
   --{ 'vim-pandoc/vim-pandoc-syntax' }, -- pandoc markdown syntax, to be installed alongside vim-pandoc
   --{ 'quarto-dev/quarto-vim', dependencies = { 'vim-pandoc/vim-pandoc-syntax' } }, -- rmarkdown support for vim
-  --{ 'akinsho/toggleterm.nvim', config = function() require('mrv.plugins.toggleterm') end }, -- a neovim lua plugin to help easily manage multiple terminal windows
   --{ 'mroavi/lf.vim', config = function() require('mrv.plugins.lf-vim') end }, -- file manager for vim/neovim powered by lf
-  --{ 'vladdoster/remember.nvim', config = [[ require('remember') ]] }, -- remembers cursor position
   --{ 'romainl/vim-cool' }, -- disables search highlighting when you are done searching and re-enables it when you search again
-  --{ '~/repos/goyo.vim', config = function() vim.cmd("exe 'source ~/.config/nvim/lua/mrv/plugins/goyo.vim'") end }, -- distraction-free writing in Vim
+  --{ 'mroavi/goyo.vim', dev = true, config = function() vim.cmd("exe 'source ~/.config/nvim/lua/mrv/plugins/goyo.vim'") end }, -- distraction-free writing in Vim
   --{ 'edluffy/hologram.nvim', config = function() require('mrv.plugins.hologram') end }, -- a cross platform terminal image viewer for Neovim
   --{ 'junegunn/limelight.vim', config = function() require('mrv.plugins.limelight') end }, -- all the world's indeed a stage and we are merely players
   --{ 'folke/zen-mode.nvim' , config = function() require('mrv.plugins.zen-mode') end }, -- distraction-free coding for Neovim
-  --{ 'JuliaEditorSupport/julia-vim', config = function() require('mrv.plugins.julia-vim') end }, -- vim support for Julia
   --{ 'amarakon/nvim-cmp-lua-latex-symbols' }, -- nvim-cmp source for LaTeX symbols (100% Lua)
   --{ 'hrsh7th/cmp-emoji' }, -- nvim-cmp source for emoji
-  --{ 'nvim-lua/plenary.nvim', config = function() require('mrv.plugins.plenary') end }, -- all the lua functions you don't want to write twice (I use it to reload my nvim config)
-  --{ 'jackMort/ChatGPT.nvim' , config = function() require('mrv.plugins.chatgpt') end, dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" } }, -- plugin for interacting with OpenAI GPT-3 chatbot
-  --{ dir = '~/repos/tender.nvim', config = function() vim.cmd.colorscheme("tender") end }, -- my color scheme
   --{ 'rose-pine/neovim', name = 'rose-pine', lazy = false, priority = 1000, config = function() require('mrv.plugins.rose-pine') end },
-  --{ 'L3MON4D3/LuaSnip', config = function() require('mrv.plugins.luasnip') end },
-  --{ 'wellle/targets.vim', config = function() require('mrv.plugins.targets') end }, -- vim plugin that provides additional text objects
   --{ 'nvim-treesitter/playground', config = function() require('mrv.plugins.playground') end, dependencies = { 'nvim-treesitter/nvim-treesitter' } }, -- view treesitter information directly in Neovim
-  --{ 'nvim-neorg/neorg', config = function() require('mrv.plugins.neorg') end, build = ':Neorg sync-parsers', dependencies = { { 'nvim-lua/plenary.nvim' } } }, -- the future of organizing your life in Neovim
-  --{ "lukas-reineke/indent-blankline.nvim", config = function() require('mrv.plugins.indent-blankline') end }, -- indent guides for Neovim
-  --{ "echasnovski/mini.splitjoin", keys = 'gS', config = function() require('mrv.plugins.mini-splitjoin') end }, -- Neovim Lua plugin to split and join arguments. Part of 'mini.nvim' library.
-  --{ "bennypowers/splitjoin.nvim", config = function() require('mrv.plugins.splitjoin') end }, -- split or join list-like syntax constructs
+  --{ 'lukas-reineke/indent-blankline.nvim', config = function() require('mrv.plugins.indent-blankline') end }, -- indent guides for Neovim
   --{ 'folke/which-key.nvim', config = function() require('mrv.plugins.which-key') end }, -- displays a popup with possible keybindings of the command you started typing
   --{ 'mroavi/slides.vim', dev = true, config = function() require('mrv.plugins.slides') end, dependencies = { 'edluffy/hologram.nvim', 'junegunn/limelight.vim', 'tpope/vim-obsession' } }, -- presentation slides in vim
+  --{ 'kaarmu/typst.vim' }, -- Vim plugin for Typst
 
   -- Try these out!
   --{ 'famiu/nvim-reload' }, -- plugin to easily reload your Neovim config
   --{ 'kassio/neoterm' }, -- wrapper of some vim/neovim's :terminal functions
   --{ 'sindrets/diffview.nvim' }, -- single tabpage interface for easily cycling through diffs for all modified files for any git rev
   --{ 'ray-x/lsp_signature.nvim', config = function() require('mrv.plugins.lsp_signature') end }, -- LSP signature hint as you type
-  --{ "glepnir/lspsaga.nvim" }, -- a light-weight lsp plugin based on neovim built-in lsp with highly a performant UI
+  --{ 'glepnir/lspsaga.nvim' }, -- a light-weight lsp plugin based on neovim built-in lsp with highly a performant UI
   --{ 'terrortylor/nvim-comment', config = function() require('mrv.plugins.nvim-comment') end }, -- a comment toggler for Neovim, written in Lua
   --{ 'chipsenkbeil/distant.nvim', }, -- edit files, run programs, and work with LSP on a remote machine from the comfort of your local environment construction
   --{ 'rcarriga/nvim-notify', }, -- a fancy, configurable, notification manager for NeoVim
@@ -93,7 +80,7 @@ local plugins = {
   --{ 'tpope/vim-rhubarb' }, -- GitHub extension for fugitive.vim
   --{ 'smjonas/live-command.nvim' }, -- the easiest way to create previewable commands in Neovim
   --{ 'xolox/vim-notes' }, -- easy note taking in Vim
-  --{ tpope/vim-vinegar }, -- combine with netrw to create a delicious salad dressing
+  --{ 'tpope/vim-vinegar' }, -- combine with netrw to create a delicious salad dressing
   --{ 'stevearc/dressing.nvim' }, -- Neovim plugin to improve the default vim.ui interfaces
   --{ 'nvim-pack/nvim-spectre' }, -- global find and replace tool
   --{ 'kevinhwang91/nvim-ufo' }, -- not UFO in the sky, but an ultra fold in Neovim
@@ -114,6 +101,19 @@ local plugins = {
   --{ 'toranb/tmux-navigator', config = function() require('mrv.plugins.tmux-navigator') end }, -- navigate seamlessly between vim and tmux splits using a set of hotkeys
   --{ 'mcchrish/nnn.vim' }, -- file manager for vim/neovim powered by n³
   --{ 'ojroques/vim-oscyank', config = function() require('mrv.plugins.vim-oscyank') end }, -- a Vim plugin to copy text through SSH with OSC52
+  --{ 'kdheepak/JuliaFormatter.vim', ft = "julia", config = function() require('mrv.plugins.JuliaFormatter') end }, -- formatter for Julia
+  --{ 'akinsho/toggleterm.nvim', config = function() require('mrv.plugins.toggleterm') end }, -- a neovim lua plugin to help easily manage multiple terminal windows
+  --{ 'vladdoster/remember.nvim', config = [[ require('remember') ]] }, -- remembers cursor position
+  --{ 'JuliaEditorSupport/julia-vim', config = function() require('mrv.plugins.julia-vim') end }, -- vim support for Julia
+  --{ 'nvim-lua/plenary.nvim', config = function() require('mrv.plugins.plenary') end }, -- all the lua functions you don't want to write twice (I use it to reload my nvim config)
+  --{ 'jackMort/ChatGPT.nvim' , config = function() require('mrv.plugins.chatgpt') end, dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" } }, -- plugin for interacting with OpenAI GPT-3 chatbot
+  --{ 'wellle/targets.vim', config = function() require('mrv.plugins.targets') end }, -- vim plugin that provides additional text objects
+  --{ 'nvim-neorg/neorg', config = function() require('mrv.plugins.neorg') end, build = ':Neorg sync-parsers', dependencies = { { 'nvim-lua/plenary.nvim' } } }, -- the future of organizing your life in Neovim
+  --{ 'echasnovski/mini.splitjoin', keys = 'gS', config = function() require('mrv.plugins.mini-splitjoin') end }, -- Neovim Lua plugin to split and join arguments. Part of 'mini.nvim' library.
+  --{ 'bennypowers/splitjoin.nvim', config = function() require('mrv.plugins.splitjoin') end }, -- split or join list-like syntax constructs
+  --{ 'preservim/vim-markdown' }, -- syntax highlighting, matching rules and mappings for Markdown and extensions
+  --{ 'dstein64/nvim-scrollview', config = function() require('mrv.plugins.scrollview') end }, -- a Neovim plugin that displays (non-interactive) scrollbars
+  --{ 'mroavi/tender.nvim', dev =true, config = function() vim.cmd.colorscheme("tender") end }, -- my color scheme
 
   -- }}}
 
