27332e961db01c3d18ff02a167624bba9868c3d6
Martin Roa Villescas <mroavi@gmail.com>
2024-05-02 21:51:44 +0200

    vim (gv): add abbr that only lists commits that affected curr buffer

--- nvim/.config/nvim/lua/mrv/plugins/gv.lua
+++ nvim/.config/nvim/lua/mrv/plugins/gv.lua
@@ -1,2 +1,7 @@
 -- Emulates zsh's glg alias in nvim's command line
+
+-- Open commit browser
 vim.cmd('cnoreabbrev glg GV')
+
+-- Only list commits that affected the current buffer
+vim.cmd('cnoreabbrev glgb GV!')
