de67f9b70b5e199e8cd579158e7bb8058f6849f0
Martin Roa Villescas <mroavi@gmail.com>
2024-05-11 01:30:05 +0200

    lf: fix indentation

--- lf/.config/lf/pv.sh
+++ lf/.config/lf/pv.sh
@@ -7,5 +7,5 @@ case "$1" in
     *.7z) 7z l "$1";;
     *.pdf) pdftotext "$1" -;;
     *) highlight -O ansi "$1" || cat "$1";;
-    # *) bat --plain --color always "$1"
+    #*) bat --plain --color always "$1"
 esac
