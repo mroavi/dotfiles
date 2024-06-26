403f345845723d32e7806b15a3cc5e6c9b5845ad
Martin Roa Villescas <mroavi@gmail.com>
2024-06-26 07:18:49 +0200

    vim (lazy): update bootstrap code

--- nvim/.config/nvim/lua/mrv/plugins/init.lua
+++ nvim/.config/nvim/lua/mrv/plugins/init.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
