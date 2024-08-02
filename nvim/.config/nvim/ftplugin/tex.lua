8c886454e77e98646456d2c20b6c52a6754a447d
Martin Roa Villescas <mroavi@gmail.com>
2024-08-02 06:05:37 +0200

    vim (quickfix): disable wrap in the quickfix list

--- nvim/.config/nvim/ftplugin/tex.lua
+++ nvim/.config/nvim/ftplugin/tex.lua
@@ -3,6 +3,9 @@
 vim.api.nvim_command([[
 function! AdjustWindowHeight(minheight, maxheight)
   exe max([min([line("$"), a:maxheight]), a:minheight]) . "wincmd _"
+  " Disable wrap in the quickfix list
+  " https://vi.stackexchange.com/a/2844/27039
+  setlocal nowrap
 endfunction
 au FileType qf call AdjustWindowHeight(3, 10)
 ]])
