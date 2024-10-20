c63d9c20a331f0dcb50843beb8724d25b59cd3ba
Martin Roa Villescas <mroavi@gmail.com>
2024-10-21 00:05:53 +0200

    wezterm: set MesloLGLDZ font with 11 pts

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,8 +8,9 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font = wezterm.font("MesloLGLDZ Nerd Font Mono")
+
+config.font_size = 11
 
 config.enable_tab_bar = false
 
