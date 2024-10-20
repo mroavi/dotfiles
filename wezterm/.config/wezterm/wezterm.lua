36a39aafec95ae9683a231520db4a2e7511c3023
Martin Roa Villescas <mroavi@gmail.com>
2024-10-20 08:09:39 +0200

    wezterm: remove unnecessary mac os specific config line

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -16,7 +16,6 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
