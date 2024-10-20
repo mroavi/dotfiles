8c2a8be6a01e67736bf86c147ca614f6772054e9
Martin Roa Villescas <mroavi@gmail.com>
2024-10-20 07:59:57 +0200

    wezterm: add config file

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 19
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- and finally, return the configuration to wezterm
+return config
