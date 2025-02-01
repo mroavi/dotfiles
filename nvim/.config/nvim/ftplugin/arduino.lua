e9865f3b6faa889d9b4d1ba68262a9b4ec1b6e26
Martin Roa Villescas <mroavi@gmail.com>
2025-02-01 17:08:55 +0100

    vim (arduino): comment out cmp code

--- nvim/.config/nvim/ftplugin/arduino.lua
+++ nvim/.config/nvim/ftplugin/arduino.lua
@@ -2,12 +2,12 @@
 --- cmp
 ---------------------------------------------------------------------------------
 
--- Disable a given source for this filetype
-local cmp = require('cmp')
-local sources = cmp.get_config().sources
-for i = #sources, 1, -1 do
-  if sources[i].name == 'buffer' then
-    table.remove(sources, i)
-  end
-end
-cmp.setup.buffer({ sources = sources })
+---- Disable a given source for this filetype
+--local cmp = require('cmp')
+--local sources = cmp.get_config().sources
+--for i = #sources, 1, -1 do
+--  if sources[i].name == 'buffer' then
+--    table.remove(sources, i)
+--  end
+--end
+--cmp.setup.buffer({ sources = sources })
