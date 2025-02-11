84121ae1f210d5a3e2f3c200423670095187ab04
Martin Roa Villescas <mroavi@gmail.com>
2025-02-11 10:33:33 +0100

    vim (lspconfig): update path to clangd bin

--- nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/mrv/plugins/lspconfig.lua
@@ -275,7 +275,7 @@ if not vim.env.SSH_CONNECTION then
   lspconfig.arduino_language_server.setup({
     cmd = {
       "arduino-language-server",
-      "-clangd", vim.fn.expand("~/.local/share/nvim/mason/packages/clangd/clangd_18.1.3/bin/clangd"), -- WARNING: this path may change after updating clangd
+      "-clangd", vim.fn.expand("~/.local/share/nvim/mason/packages/clangd/clangd_19.1.2/bin/clangd"), -- WARNING: this path may change after updating clangd
       "-cli", "/bin/arduino-cli",
       "-cli-config", vim.fn.expand("~/.arduino15/arduino-cli.yaml"),
     }
