052e06d2e4386d778c03347087bb1b44359096d3
Martin Roa Villescas <mroavi@gmail.com>
2025-05-25 00:54:51 +0200

    vim (java): config nvim-jdtls

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -69,3 +69,104 @@ end, { buffer = true })
 vim.keymap.set('n', '<Leader>tt', function()
   vim.cmd [[TomuxSend("./gradlew test\n")]]
 end, { buffer = true })
+
+---------------------------------------------------------------------------------
+--- nvim-jdtls
+---------------------------------------------------------------------------------
+
+-- Sources: 
+--  - https://youtu.be/C7juSZsM2Fg?si=cA9O4sks4KL3L6I-
+--  - https://raw.githubusercontent.com/exosyphon/nvim/refs/heads/main/ftplugin/java.lua
+
+local home = os.getenv 'HOME'
+local workspace_path = home .. '/.local/share/nvim/jdtls-workspace/'
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+local workspace_dir = workspace_path .. project_name
+
+local status, jdtls = pcall(require, 'jdtls')
+if not status then
+  return
+end
+local extendedClientCapabilities = jdtls.extendedClientCapabilities
+
+local config = {
+  cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    '-jar',
+    vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+    '-data',
+    workspace_dir,
+  },
+  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all', -- literals, all, none
+        },
+      },
+      format = {
+        enabled = false,
+      },
+    },
+  },
+
+  -- Fixed the `LSP[jdtls] Security Warning! The Gradle wrapper ... could be
+    -- malicious.` warning using the solution from:
+    -- https://github.com/mfussenegger/nvim-jdtls/discussions/249#discussioncomment-3159367
+  init_options = {
+    settings = {
+      java = {
+        implementationsCodeLens = { enabled = true },
+        imports = {
+          gradle = {
+            enabled = true,
+            wrapper = {
+              enabled = true,
+              checksums = {
+                {
+                  sha256 = '7d3a4ac4de1c32b59bc6a4eb8ecb8e612ccd0cf1ae1e99f66902da64df296172',
+                  allowed = true
+                }
+              },
+            }
+          }
+        }
+      }
+    },
+  }
+}
+require('jdtls').start_or_attach(config)
+
+--vim.keymap.set('n', '<Leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
+--vim.keymap.set('n', '<Leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
+--vim.keymap.set('v', '<Leader>crv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", { desc = 'Extract Variable' })
+--vim.keymap.set('n', '<Leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
+--vim.keymap.set('v', '<Leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", { desc = 'Extract Constant' })
+--vim.keymap.set('v', '<Leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
