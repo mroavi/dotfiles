cdfa76e2bc65192d84452cad0b58e374604a809a
Martin Roa Villescas <mroavi@gmail.com>
2025-05-29 23:31:42 +0200

    vim (javascript): add print debug utilities

--- nvim/.config/nvim/ftplugin/javascript.lua
+++ nvim/.config/nvim/ftplugin/javascript.lua
@@ -1,3 +1,18 @@
+local vim = vim
+local utils = require('mrv.utils')
+
+---------------------------------------------------------------------------------
+--- Debug utilities
+---------------------------------------------------------------------------------
+
+vim.keymap.set('n', '<Leader>pr', function()
+  utils.insert_string([[console.log(\"'%s': \" + %s); // DEBUG]], 'o')
+end, { buffer = true })
+
+vim.keymap.set('n', '<Leader>pt', function()
+  utils.insert_string([[console.table(%s); // DEBUG]], 'o')
+end, { buffer = true })
+
 -------------------------------------------------------------------------------
 -- conform
 -------------------------------------------------------------------------------
