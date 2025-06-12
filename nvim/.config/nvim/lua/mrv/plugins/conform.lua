e0b10ca22b0e6f24cc9bfb70190ae13f83ea82f4
Martin Roa Villescas <mroavi@gmail.com>
2025-06-12 22:03:43 +0200

    vim (conform): format on save for specified filetypes

--- nvim/.config/nvim/lua/mrv/plugins/conform.lua
+++ nvim/.config/nvim/lua/mrv/plugins/conform.lua
@@ -13,15 +13,15 @@ local conform = require("conform")
 conform.setup({
   -- I use Mason to install these formatters
   formatters_by_ft = {
-    python = { "black" },
-    tex = { "latexindent" },
     arduino = { "clang-format" },
     java = { "google-java-format" },
     javascript = { "prettier" },
     javascriptreact = { "prettier" },
+    json = { "prettier" },
+    python = { "black" },
+    tex = { "latexindent" },
     typescript = { "prettier" },
     typescriptreact = { "prettier" },
-    json = { "prettier" },
   },
   -- Override/add to the default values of formatters
   formatters = {
@@ -36,23 +36,41 @@ vim.keymap.set("n", "<Leader>C", ":ConformInfo<CR>")
 
 -- Ensure Conform is used for `gq` even after LSP attaches
 local conform_filetypes = {
-	javascript = true,
-	javascriptreact = true,
-	typescript = true,
-	typescriptreact = true,
-	json = true,
-	tex = true, -- for `latexindent`: pacman -S libxcrypt-compat if libcrypt.so.1 error
-	python = true,
-	java = true,
-	arduino = true,
+  arduino = true,
+  javascriptreact = true,
+  javascript = true,
+  java = true,
+  json = true,
+  python = true,
+  tex = true, -- for `latexindent`: pacman -S libxcrypt-compat if libcrypt.so.1 error
+  typescriptreact = true,
+  typescript = true,
 }
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		local buf = args.buf
-		local ft = vim.bo[buf].filetype
-		if conform_filetypes[ft] then
-			vim.bo[buf].formatexpr = "v:lua.require'conform'.formatexpr()"
-		end
-	end,
+  callback = function(args)
+    local buf = args.buf
+    local ft = vim.bo[buf].filetype
+    if conform_filetypes[ft] then
+      vim.bo[buf].formatexpr = "v:lua.require'conform'.formatexpr()"
+    end
+  end,
+})
+
+-- Autoformat-on-save only for these filetypes
+local autoformat_filetypes = {
+  javascriptreact = true,
+  javascript = true,
+  typescriptreact = true,
+  typescript = true,
+}
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    if autoformat_filetypes[ft] then
+      conform.format({ bufnr = args.buf })
+    end
+  end,
 })
