61fa4bab6660c138e879b0c59aad7c2201f411b1
Martin Roa Villescas <mroavi@gmail.com>
2025-09-25 22:10:07 +0200

    vim (javascript): use relative paths with execute mapping

--- nvim/.config/nvim/ftplugin/javascript.lua
+++ nvim/.config/nvim/ftplugin/javascript.lua
@@ -28,16 +28,21 @@ vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
 
 vim.g.tomux_use_clipboard = 0
 
+---- Start interpreter cmd
+--vim.b.start_interpreter_cmd = 'node'
+
 -- Open a pane to the right, starting in Neovim's current working directory
 vim.keymap.set('n', '<Leader>tl', function()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
   vim.cmd([[TomuxCommand("split-window -h -d -c '" . getcwd() . "'")]])
+  --vim.cmd([[TomuxSend(b:start_interpreter_cmd . "\n")]])
 end, { buffer = true })
 
 -- Open a pane below, starting in Neovim's current working directory
 vim.keymap.set('n', '<Leader>tj', function()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{down-of}" }
   vim.cmd([[TomuxCommand("split-window -v -d -l 20% -c '" . getcwd() . "'")]])
+  --vim.cmd([[TomuxSend(b:start_interpreter_cmd . "\n")]])
 end, { buffer = true })
 
 -- Kill pane
@@ -48,10 +53,17 @@ end, { buffer = true })
 -- Run (execute)
 vim.keymap.set('n', '<Leader>e', function()
   vim.cmd.write()
-  vim.cmd [[TomuxSend("node " . expand('%:p'). "\n")]]
+  vim.cmd [[TomuxSend("node " . expand('%:.'). "\n")]]
+  --vim.cmd [[TomuxSend(".load " . expand('%:.'). "\n")]]
 end, { buffer = true })
 
 -- Test
 vim.keymap.set('n', '<Leader>tt', function()
   vim.cmd [[TomuxSend("node --test\n")]]
 end, { buffer = true })
+
+-- Clear command line
+function M.clear()
+  vim.cmd[[TomuxCommand("send-keys -t " . shellescape(b:tomux_config["target_pane"]) . " C-l")]]
+end
+vim.keymap.set('n', '<C-l>', function() M.clear() end, { buffer = true })
