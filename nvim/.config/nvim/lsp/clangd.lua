b5379b83bc78815d065a96f409c32cff269cda69
Martin Roa Villescas <mroavi@gmail.com>
2025-09-28 13:12:37 +0000

    vim (clangd): add notes on how to generate the compile_commands.json

--- nvim/.config/nvim/lsp/clangd.lua
+++ nvim/.config/nvim/lsp/clangd.lua
@@ -5,5 +5,11 @@ return {
 }
 
 -- Use `bear` to generate the `compile_commands.json` file needed by clangd
+-- when the project does NOT use CMake.
 -- Installation: yay -S bear
 -- Github page: https://github.com/rizsotto/Bear
+--
+-- For CMake-based projects, the recommended approach is:
+--   cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+-- and then symlink the generated file into the project root:
+--   ln -s build/compile_commands.json compile_commands.json
