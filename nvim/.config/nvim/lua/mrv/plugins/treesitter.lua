3a80fd640bc40578e291ad1e4415942ab5f446f7
Martin Roa Villescas <mroavi@gmail.com>
2026-04-27 16:55:40 +0200

    vim (treesitter): fix breaking change

--- nvim/.config/nvim/lua/mrv/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mrv/plugins/treesitter.lua
@@ -1,107 +1,37 @@
--- Run :TSInstall comment to enable syntax highlighting for the words ERROR, WARN, NOTE, and TODO
+-- Treesitter configuration
+require('nvim-treesitter').setup()
 
-require 'nvim-treesitter.configs'.setup {
+-- Install parsers (runs async, safe to call directly)
+require('nvim-treesitter').install({
+  "arduino",
+  "bash",
+  "c",
+  "comment",
+  "cpp",
+  "c_sharp",
+  "css",
+  "html",
+  "java",
+  "javascript",
+  "json",
+  "julia",
+  "lua",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "regex",
+  "rust",
+  "sql",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+})
 
-  -- One of "all" or a list of languages
-  ensure_installed = {
-    "arduino",
-    "c",
-    "cpp",
-    "c_sharp",
-    "css",
-    "dockerfile",
-    "html",
-    "java",
-    "javascript",
-    "json",
-    "julia",
-    "lua",
-    "python",
-    "rust",
-    "sql",
-    "tsx", -- also supports .jsx files
-    "typescript",
-    "vim",
-  },
-
-  -- Install languages synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- List of parsers to ignore installing
-  ignore_install = {},
-
-  highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
-
-    -- List of languages that will be disabled
-    disable = {
-      "dockerfile",
-      --"julia",
-      --"help",
-      --"markdown",
-      --"latex",
-      --"gitcommit",
-      --"arduino",
-    },
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-
-  indent = {
-    enable = true,
-    -- List of filetypes for which treesitter-based indentation is disabled
-    disable = {
-      "markdown",
-      --"python",
-      --"yaml",
-      --"arduino",
-    },
-  },
-
-  -- Incremental selection based on the named nodes from the grammar
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'van',
-      node_incremental = '.',
-      node_decremental = ',',
-    },
-  },
-
-  ---- Text objects extension (https://github.com/nvim-treesitter/nvim-treesitter-textobjects)
-  --textobjects = {
-  --  select = {
-  --    enable = true,
-  --    lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
-  --    keymaps = {
-  --      -- You can use the capture groups defined in textobjects.scm
-  --      ['aa'] = '@parameter.outer',
-  --      ['ia'] = '@parameter.inner',
-  --      ['af'] = '@function.outer',
-  --      ['if'] = '@function.inner',
-  --    },
-  --  },
-  --  move = {
-  --    enable = true,
-  --    set_jumps = true, -- whether to add jumps to the jumplist
-  --    goto_next_start = {
-  --      [']]'] = '@function.outer',
-  --    },
-  --    goto_next_end = {
-  --      [']['] = '@function.outer',
-  --    },
-  --    goto_previous_start = {
-  --      ['[['] = '@function.outer',
-  --    },
-  --    goto_previous_end = {
-  --      ['[]'] = '@function.outer',
-  --    },
-  --  },
-  --},
-
-}
+-- Disable treesitter indent for specific languages
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    vim.bo.indentexpr = ""
+  end,
+})
