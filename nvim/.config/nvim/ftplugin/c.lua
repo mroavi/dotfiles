55473ed4a7bf77864a1bda4536bca899f2eeccf1
Martin Roa Villescas <mroavi@gmail.com>
2026-05-15 23:40:24 +0200

    vim (c): add tomux profiles and add mappings for PlatformIO

--- nvim/.config/nvim/ftplugin/c.lua
+++ nvim/.config/nvim/ftplugin/c.lua
@@ -39,6 +39,41 @@ vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
 -- Send text as if it were typed
 vim.g.tomux_use_clipboard = 0
 
+-- Build profiles: each action is { key, cmd }. The key is the <Leader>-mapping
+-- that triggers the command.
+local profile = 'pio'
+local profiles = {
+  make = {
+    build = { '<Leader>tm', 'make' },
+    run   = { '<Leader>e',  'make && ./build/apps/program' },
+    test  = { '<Leader>tt', 'make && bash peak-mem-usage.sh' },
+    clean = { '<Leader>tc', 'make clean' },
+  },
+  pio = {
+    build        = { '<Leader>tr', 'pio run' },
+    upload       = { '<Leader>tu', 'pio run -t nobuild -t upload' },
+    monitor      = { '<Leader>tm', 'pio device monitor' },
+    build_upload = { '<Leader>e',  'pio run -t upload' },
+    all          = { '<Leader>tt', 'pio run -t upload -t monitor' },
+    clean        = { '<Leader>tc', 'pio run -t clean' },
+    erase        = { '<Leader>tE', 'pio run -t erase' },
+    size         = { '<Leader>ts', 'pio run -t size' },
+  },
+  cmake = {
+    build = { '<Leader>tm', 'cmake --build build' },
+    run   = { '<Leader>e',  'cmake --build build && ./build/apps/program' },
+    test  = { '<Leader>tt', 'cmake --build build && ctest --test-dir build' },
+    clean = { '<Leader>tc', 'cmake --build build --target clean' },
+  },
+}
+local active = profiles[profile]
+
+-- Send a command to the tmux pane after saving the buffer
+local function send(cmd)
+  vim.cmd.update()
+  vim.cmd(string.format([[TomuxSend("%s\n")]], cmd))
+end
+
 -- Open a pane to the right, starting in Neovim's current working directory
 vim.keymap.set('n', '<Leader>tl', function()
   vim.b.tomux_config = { socket_name = "default", target_pane = "{right-of}" }
@@ -51,29 +86,19 @@ vim.keymap.set('n', '<Leader>tj', function()
   vim.cmd([[TomuxCommand("split-window -v -d -l 20% -c '" . getcwd() . "'")]])
 end, { buffer = true })
 
--- Make
-vim.keymap.set('n', '<Leader>tm', function()
-  vim.cmd.update()
-  vim.cmd [[TomuxSend("make\n")]]
-end, { buffer = true })
-
--- Make and execute
-vim.keymap.set('n', '<Leader>e', function()
-  vim.cmd.update()
-  vim.cmd [[TomuxSend("make && ./build/apps/program\n")]] -- WARNING: update the path to the executable
-end, { buffer = true })
-
--- Test
-vim.keymap.set('n', '<Leader>tt', function()
-  vim.cmd.update()
-  vim.cmd [[TomuxSend("make && bash peak-mem-usage.sh\n")]] -- WARNING: update command as needed
-end, { buffer = true })
-
--- Clean
-vim.keymap.set('n', '<Leader>tc', function()
-  vim.cmd.update()
-  vim.cmd [[TomuxSend("make clean\n")]]
-end, { buffer = true })
+-- Register a keymap for each action in the active profile
+local seen = {}
+for name, action in pairs(active) do
+  local key, cmd = action[1], action[2]
+  if seen[key] then
+    vim.notify(
+      ("build profile: key %s used by both %s and %s"):format(key, seen[key], name),
+      vim.log.levels.WARN
+    )
+  end
+  seen[key] = name
+  vim.keymap.set('n', key, function() send(cmd) end, { buffer = true })
+end
 
 -- Kill pane
 vim.keymap.set('n', '<Leader>tk', function()
